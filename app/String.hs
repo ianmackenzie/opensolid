fc570afbedfdfdc75595a3e13fc55c71b9d51ee5
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-09-02 21:27:45 -0400

    Add some more basic String functions

--- app/String.hs
+++ app/String.hs
@@ -1,9 +1,30 @@
 module String (
     concat,
+    join,
+    fromList,
+    toList,
+    fromInt,
+    fromFloat,
 ) where
 
 import qualified Data.Text
 import OpenSolid
+import qualified Prelude
 
 concat :: List String -> String
 concat = Data.Text.concat
+
+join :: String -> List String -> String
+join = Data.Text.intercalate
+
+fromList :: List Char -> String
+fromList = Data.Text.pack
+
+toList :: String -> List Char
+toList = Data.Text.unpack
+
+fromInt :: Int -> String
+fromInt n = fromList (Prelude.show n)
+
+fromFloat :: Float -> String
+fromFloat x = fromList (Prelude.show x)
