1a3c1841995d21512efc82de6be1cdfb22d48520
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-09-09 17:22:20 -0400

    Simplify Debug.toString implementation slightly
    Try to reduce number of places in the code that use Data.Text directly

--- app/Debug.hs
+++ app/Debug.hs
@@ -1,6 +1,5 @@
 module Debug (log, toString) where
 
-import qualified Data.Text
 import qualified Data.Text.IO
 import OpenSolid
 import qualified String
@@ -12,4 +11,4 @@ log label value =
 
 toString :: Show a => a -> String
 toString value =
-    Data.Text.pack (show value)
+    String.fromList (show value)
