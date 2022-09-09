0b921d1dd50b9911fd1692202d63c136f606e83f
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-09-09 19:29:58 -0400

    Add Curve3d.[pointOn, bisect]

--- app/Curve3d.hs
+++ app/Curve3d.hs
@@ -3,11 +3,14 @@ module Curve3d (
     IsCurve3d (..),
 ) where
 
+import OpenSolid
 import Point3d (Point3d)
 
 class IsCurve3d curve where
     startPoint :: curve coordinates -> Point3d coordinates
     endPoint :: curve coordinates -> Point3d coordinates
+    pointOn :: curve coordinates -> Float -> Point3d coordinates
+    bisect :: curve coordinates -> (curve coordinates, curve coordinates)
 
 data Curve3d coordinates = forall curve. IsCurve3d curve => Curve3d (curve coordinates)
 
@@ -17,3 +20,10 @@ instance IsCurve3d Curve3d where
 
     endPoint (Curve3d curve) =
         endPoint curve
+
+    pointOn (Curve3d curve) t =
+        pointOn curve t
+
+    bisect (Curve3d curve) =
+        let (curve1, curve2) = bisect curve
+         in (Curve3d curve1, Curve3d curve2)
