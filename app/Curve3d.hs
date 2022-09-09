796a7b1b55c69892c7d13c3473e9b4cc4844024a
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-09-09 17:42:13 -0400

    Add skeleton Curve3d module

---
+++ app/Curve3d.hs
@@ -0,0 +1,21 @@
+module Curve3d (
+    Curve3d (..),
+    IsCurve3d (..),
+) where
+
+import Point3d (Point3d)
+
+class IsCurve3d curve where
+    startPoint :: curve coordinates -> Point3d coordinates
+    endPoint :: curve coordinates -> Point3d coordinates
+
+data Curve3d coordinates = forall curve. IsCurve3d curve => Curve3d (curve coordinates)
+
+instance IsCurve3d Curve3d where
+    startPoint :: Curve3d coordinates -> Point3d coordinates
+    startPoint (Curve3d curve) =
+        startPoint curve
+
+    endPoint :: Curve3d coordinates -> Point3d coordinates
+    endPoint (Curve3d curve) =
+        endPoint curve
