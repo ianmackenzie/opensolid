de9623c9ef1f6e2058cff1092c0a1cd7b42599da
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-09-09 22:44:23 -0400

    Add initial QuadraticSpline3d module

---
+++ app/QuadraticSpline3d.hs
@@ -0,0 +1,30 @@
+module QuadraticSpline3d (QuadraticSpline3d (..)) where
+
+import Curve3d (IsCurve3d (..))
+import Point3d (Point3d)
+import qualified Point3d
+
+data QuadraticSpline3d coordinates
+    = QuadraticSpline3d
+        (Point3d coordinates)
+        (Point3d coordinates)
+        (Point3d coordinates)
+
+instance IsCurve3d QuadraticSpline3d where
+    startPoint (QuadraticSpline3d p1 _ _) =
+        p1
+
+    endPoint (QuadraticSpline3d _ _ p3) =
+        p3
+
+    pointOn (QuadraticSpline3d p1 p2 p3) t =
+        Point3d.interpolateFrom
+            (Point3d.interpolateFrom p1 p2 t)
+            (Point3d.interpolateFrom p2 p3 t)
+            t
+
+    bisect (QuadraticSpline3d p1 p2 p3) =
+        let q1 = Point3d.midpoint p1 p2
+            q2 = Point3d.midpoint p2 p3
+            r = Point3d.midpoint q1 q2
+         in (QuadraticSpline3d p1 q1 r, QuadraticSpline3d r q2 p3)
