2b4c7a56cadeba3ec4ede6cdd00c261dfd177043
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-10-11 08:43:50 -0400

    Add several more List functions

--- app/List.hs
+++ app/List.hs
@@ -1,8 +1,45 @@
-module List (map) where
+module List (
+    map,
+    compact,
+    collect,
+    combine,
+    foldl,
+    foldr,
+    reverse,
+) where
 
+import qualified Data.List
+import qualified Data.Maybe
 import OpenSolid
-import qualified Prelude
 
 map :: (a -> b) -> List a -> List b
 map =
-    Prelude.fmap
+    Data.List.map
+
+compact :: List (Maybe a) -> List a
+compact =
+    Data.Maybe.catMaybes
+
+collect :: (a -> Maybe b) -> List a -> List b
+collect =
+    Data.Maybe.mapMaybe
+
+combine :: (a -> List b) -> List a -> List b
+combine function list =
+    list >>= function
+
+concat :: List (List a) -> List a
+concat =
+    Data.List.concat
+
+foldl :: (b -> a -> b) -> b -> List a -> b
+foldl =
+    Data.List.foldl'
+
+foldr :: (a -> b -> b) -> b -> List a -> b
+foldr =
+    Data.List.foldr
+
+reverse :: List a -> List a
+reverse =
+    Data.List.reverse
