f82a63738dee017edb2ed3d7bde0fed83c0d50cb
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-10-11 08:45:50 -0400

    Add Maybe module with basic functions

---
+++ app/Maybe.hs
@@ -0,0 +1,23 @@
+module Maybe (
+    map,
+    map2,
+    withDefault,
+) where
+
+import qualified Data.Maybe
+import OpenSolid
+import qualified Prelude
+
+map :: (a -> b) -> Maybe a -> Maybe b
+map =
+    Prelude.fmap
+
+map2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+map2 function maybeA maybeB = do
+    valueA <- maybeA
+    valueB <- maybeB
+    Just (function valueA valueB)
+
+withDefault :: a -> Maybe a -> a
+withDefault value maybe =
+    Data.Maybe.fromMaybe value maybe
