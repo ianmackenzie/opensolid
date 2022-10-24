31c0d564df7d20879221d87756ace7d79e5cba48
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-10-24 16:32:17 -0400

    Add missing functions to Length module

--- app/Length.hs
+++ app/Length.hs
@@ -1,5 +1,13 @@
 module Length (
     zero,
+    positiveInfinity,
+    negativeInfinity,
+    infinity,
+    isNaN,
+    interpolateFrom,
+    midpoint,
+    abs,
+    clamp,
     meter,
     centimeter,
     meters,
@@ -15,6 +23,38 @@ import UnitCoercion
 zero :: Length
 zero =
     Scalar.zero
+
+positiveInfinity :: Length
+positiveInfinity =
+    Scalar.positiveInfinity
+
+negativeInfinity :: Length
+negativeInfinity =
+    Scalar.negativeInfinity
+
+infinity :: Length
+infinity =
+    Scalar.infinity
+
+isNaN :: Length -> Bool
+isNaN =
+    Scalar.isNaN
+
+interpolateFrom :: Length -> Length -> Float -> Length
+interpolateFrom =
+    Scalar.interpolateFrom
+
+midpoint :: Length -> Length -> Length
+midpoint =
+    Scalar.midpoint
+
+abs :: Length -> Length
+abs =
+    Scalar.abs
+
+clamp :: Length -> Length -> Length -> Length
+clamp =
+    Scalar.clamp
 
 meter :: Length
 meter =
