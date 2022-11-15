25e30be1c93e0f51fd69542bf6a3db934ec6fac6
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-11-14 19:40:33 -0500

    Tweak naming

--- app/Range/Unsafe.hs
+++ app/Range/Unsafe.hs
@@ -14,42 +14,42 @@ instance Negation (Range (Qty a)) where
     negate (Range low high) =
         Range (negate high) (negate low)
 
-instance Addition Range Range Range (Qty units) where
+instance Addition Range Range Range (Qty a) where
     (Range low1 high1) + (Range low2 high2) =
         Range (low1 + low2) (high1 + high2)
 
-instance Subtraction Range Range Range (Qty units) where
+instance Subtraction Range Range Range (Qty a) where
     (Range low1 high1) - (Range low2 high2) =
         Range (low1 - high2) (high1 - low2)
 
-scalarRangeMultiplication :: Multiplication (Qty a) (Qty b) (Qty c) => Qty a -> Range (Qty b) -> Range (Qty c)
-scalarRangeMultiplication value (Range low high) =
+qtyRangeMultiplication :: Multiplication (Qty a) (Qty b) (Qty c) => Qty a -> Range (Qty b) -> Range (Qty c)
+qtyRangeMultiplication value (Range low high) =
     if value >= Qty.zero
         then Range (value * low) (value * high)
         else Range (value * high) (value * low)
 
-rangeScalarMultiplication :: Multiplication (Qty a) (Qty b) (Qty c) => Range (Qty a) -> Qty b -> Range (Qty c)
-rangeScalarMultiplication (Range low high) value =
+rangeQtyMultiplication :: Multiplication (Qty a) (Qty b) (Qty c) => Range (Qty a) -> Qty b -> Range (Qty c)
+rangeQtyMultiplication (Range low high) value =
     if value >= Qty.zero
         then Range (low * value) (high * value)
         else Range (high * value) (low * value)
 
-scalarRangeDivision :: Division (Qty a) (Qty b) (Qty c) => Qty a -> Range (Qty b) -> Range (Qty c)
-scalarRangeDivision n (Range dl dh)
+qtyRangeDivision :: Division (Qty a) (Qty b) (Qty c) => Qty a -> Range (Qty b) -> Range (Qty c)
+qtyRangeDivision n (Range dl dh)
     | dl > Qty.zero || dh < Qty.zero = Range (n / dh) (n / dl)
     | otherwise = Range Qty.negativeInfinity Qty.positiveInfinity
 
-rangeScalarDivision :: Division (Qty a) (Qty b) (Qty c) => Range (Qty a) -> Qty b -> Range (Qty c)
-rangeScalarDivision (Range nl nh) d
+rangeQtyDivision :: Division (Qty a) (Qty b) (Qty c) => Range (Qty a) -> Qty b -> Range (Qty c)
+rangeQtyDivision (Range nl nh) d
     | d > Qty.zero = Range (nl / d) (nh / d)
     | d < Qty.zero = Range (nh / d) (nl / d)
     | otherwise = Range Qty.negativeInfinity Qty.positiveInfinity
 
 instance Multiplication (Qty a) (Qty b) (Qty c) => Multiplication (Qty a) (Range (Qty b)) (Range (Qty c)) where
-    (*) = scalarRangeMultiplication
+    (*) = qtyRangeMultiplication
 
 instance Multiplication (Qty a) (Qty b) (Qty c) => Multiplication (Range (Qty a)) (Qty b) (Range (Qty c)) where
-    (*) = rangeScalarMultiplication
+    (*) = rangeQtyMultiplication
 
 instance Multiplication (Qty a) (Qty b) (Qty c) => Multiplication (Range (Qty a)) (Range (Qty b)) (Range (Qty c)) where
     (Range low1 high1) * (Range low2 high2) =
@@ -62,10 +62,10 @@ instance Multiplication (Qty a) (Qty b) (Qty c) => Multiplication (Range (Qty a)
          in Range low high
 
 instance Division (Qty a) (Qty b) (Qty c) => Division (Qty a) (Range (Qty b)) (Range (Qty c)) where
-    (/) = scalarRangeDivision
+    (/) = qtyRangeDivision
 
 instance Division (Qty a) (Qty b) (Qty c) => Division (Range (Qty a)) (Qty b) (Range (Qty c)) where
-    (/) = rangeScalarDivision
+    (/) = rangeQtyDivision
 
 instance (Division (Qty a) (Qty b) (Qty c)) => Division (Range (Qty a)) (Range (Qty b)) (Range (Qty c)) where
     (Range nl nh) / (Range dl dh)
