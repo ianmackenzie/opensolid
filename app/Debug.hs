55e37db661a2fa0d1dc98851d58d7f9eede8e337
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2022-11-25 14:00:10 -0500

    Add some debug logging functions

--- app/Debug.hs
+++ app/Debug.hs
@@ -1,8 +1,21 @@
-module Debug (toString) where
+module Debug (
+    toString,
+    trace,
+    log,
+) where
 
+import qualified Debug.Trace
 import OpenSolid
 import qualified String
 
 toString :: Show a => a -> String
 toString value =
     String.fromList (show value)
+
+trace :: String -> a -> a
+trace message value =
+    Debug.Trace.trace (String.toList message) value
+
+log :: Show a => String -> a -> a
+log label value =
+    trace (label ++ ": " ++ toString value) value
