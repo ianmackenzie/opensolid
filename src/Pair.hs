fe0050ee0de739a751008699a99daa6fc7eb7f70
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-01-08 14:20:00 -0500

    Add simple Pair module

---
+++ src/Pair.hs
@@ -0,0 +1,18 @@
+module Pair (
+    first,
+    second,
+    mapFirst,
+    mapSecond,
+) where
+
+first :: (a, b) -> a
+first (a, _) = a
+
+second :: (a, b) -> b
+second (_, b) = b
+
+mapFirst :: (a1 -> a2) -> (a1, b) -> (a2, b)
+mapFirst f (a, b) = (f a, b)
+
+mapSecond :: (b1 -> b2) -> (a, b1) -> (a, b2)
+mapSecond f (a, b) = (a, f b)
