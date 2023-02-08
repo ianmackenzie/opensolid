35c628f73f158949b3bc840b439664507ef6a3a3
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-02-07 20:22:05 -0500

    Add Axis2d.[x,y]

--- src/Axis2d.hs
+++ src/Axis2d.hs
@@ -1,5 +1,7 @@
 module Axis2d
   ( Axis2d
+  , x
+  , y
   , through
   , direction
   , originPoint
@@ -7,8 +9,10 @@ module Axis2d
 where
 
 import Direction2d (Direction2d)
+import Direction2d qualified
 import OpenSolid
 import Point2d (Point2d)
+import Point2d qualified
 import Transform2d (NoScale, Transformation2d (..))
 
 type role Axis2d nominal nominal
@@ -27,6 +31,12 @@ instance
     Axis2d
       (apply transformation (originPoint axis))
       (apply transformation (direction axis))
+
+x :: Axis2d coordinates units
+x = Axis2d Point2d.origin Direction2d.x
+
+y :: Axis2d coordinates units
+y = Axis2d Point2d.origin Direction2d.y
 
 through :: Point2d coordinates units -> Direction2d coordinates -> Axis2d coordinates units
 through = Axis2d
