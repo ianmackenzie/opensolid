07eceb589ef0de2611abdcf036eaf9b5ad833117
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-05-10 16:57:11 -0400

    Tweak formatting

--- src/Point3d.hs
+++ src/Point3d.hs
@@ -33,7 +33,6 @@ data Point3d (coordinateSystem :: CoordinateSystem) = Point3d
   }
   deriving (Eq, Show)
 
-instance (space ~ space', units ~ units') => Addition (Point3d (space @ units)) (Vector3d (space' @ units')) (Point3d (space @ units)) where
 instance
   (units1 ~ units1', units2 ~ units2', space ~ space')
   => Units.Coercion
@@ -42,12 +41,31 @@ instance
       (Point3d (space @ units1'))
       (Point3d (space' @ units2'))
 
+instance
+  (space ~ space', units ~ units')
+  => Addition
+      (Point3d (space @ units))
+      (Vector3d (space' @ units'))
+      (Point3d (space @ units))
+  where
   Point3d px py pz + Vector3d vx vy vz = Point3d (px + vx) (py + vy) (pz + vz)
 
-instance (space ~ space', units ~ units') => Subtraction (Point3d (space @ units)) (Vector3d (space' @ units')) (Point3d (space @ units)) where
+instance
+  (space ~ space', units ~ units')
+  => Subtraction
+      (Point3d (space @ units))
+      (Vector3d (space' @ units'))
+      (Point3d (space @ units))
+  where
   Point3d px py pz - Vector3d vx vy vz = Point3d (px - vx) (py - vy) (pz - vz)
 
-instance (space ~ space', units ~ units') => Subtraction (Point3d (space @ units)) (Point3d (space' @ units')) (Vector3d (space @ units)) where
+instance
+  (space ~ space', units ~ units')
+  => Subtraction
+      (Point3d (space @ units))
+      (Point3d (space' @ units'))
+      (Vector3d (space @ units))
+  where
   Point3d x1 y1 z1 - Point3d x2 y2 z2 = Vector3d (x1 - x2) (y1 - y2) (z1 - z2)
 
 instance
