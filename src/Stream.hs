15ac4d1ce269a710c6f9ea25622b0fb97dc21fce
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-06-22 16:52:26 -0400

    Add Stream module/type
    For modelling guaranteed-infinite lists

---
+++ src/Stream.hs
@@ -0,0 +1,34 @@
+module Stream
+  ( Stream (..)
+  , iterate
+  , head
+  , tail
+  , nth
+  , map
+  , take
+  )
+where
+
+import OpenSolid
+
+data Stream a = Stream a ~(Stream a)
+
+iterate :: a -> (a -> a) -> Stream a
+iterate first function = Stream first (iterate (function first) function)
+
+head :: Stream a -> a
+head (Stream first _) = first
+
+tail :: Stream a -> Stream a
+tail (Stream _ rest) = rest
+
+nth :: Int -> Stream a -> a
+nth n stream | n <= 0 = head stream
+nth n stream = nth (n - 1) (tail stream)
+
+map :: (a -> b) -> Stream a -> Stream b
+map function (Stream first rest) = Stream (function first) (map function rest)
+
+take :: Int -> Stream a -> List a
+take n _ | n <= 0 = []
+take n stream = head stream : take (n - 1) (tail stream)
