44b69f456cb49b44ba1dc03e7591d222f2207b9b
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-07-08 22:10:08 -0400

    Fix precedence of != operator

--- src/Tolerance.hs
+++ src/Tolerance.hs
@@ -15,10 +15,10 @@ type Tolerance units = ?tolerance :: Qty units
 class ApproximateEquality a b units | a -> units, b -> units where
   (~=) :: Tolerance units => a -> b -> Bool
 
-infix 4 ~=
-
 instance units ~ units' => ApproximateEquality (Qty units) (Qty units') units where
   x ~= y = Qty.abs (x - y) <= ?tolerance
 
 (!=) :: (ApproximateEquality a b units, Tolerance units) => a -> b -> Bool
 (!=) first second = not (first ~= second)
+
+infix 4 ~=, !=
