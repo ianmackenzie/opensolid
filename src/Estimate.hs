68814613e4a5d46a6f594c44b61abe9880e76f82
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-07-22 00:04:15 -0400

    Add Estimate.exact

--- src/Estimate.hs
+++ src/Estimate.hs
@@ -1,19 +1,29 @@
 module Estimate
   ( Estimate (Estimate)
   , IsEstimate (..)
+  , exact
   , bounds
   , refine
   )
 where
 
+import OpenSolid
 import Range (Range)
+import Range qualified
 
 class IsEstimate a units | a -> units where
   boundsImpl :: a -> Range units
   refineImpl :: a -> a
 
+instance IsEstimate (Qty units) units where
+  boundsImpl = Range.constant
+  refineImpl = identity
+
 data Estimate units where
   Estimate :: IsEstimate a units => a -> Estimate units
+
+exact :: Qty units -> Estimate units
+exact value = Estimate value
 
 bounds :: Estimate units -> Range units
 bounds (Estimate estimate) = boundsImpl estimate
