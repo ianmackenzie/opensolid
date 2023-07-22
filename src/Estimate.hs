27a733d19e264ec84a8bea4889c212473ea4e576
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-07-22 00:12:27 -0400

    Add Estimate.sum

--- src/Estimate.hs
+++ src/Estimate.hs
@@ -4,10 +4,13 @@ module Estimate
   , exact
   , bounds
   , refine
+  , sum
   )
 where
 
+import NonEmpty qualified
 import OpenSolid
+import Qty qualified
 import Range (Range)
 import Range qualified
 
@@ -30,3 +33,26 @@ bounds (Estimate estimate) = boundsImpl estimate
 
 refine :: Estimate units -> Estimate units
 refine (Estimate estimate) = Estimate (refineImpl estimate)
+
+data Sum units = Sum (NonEmpty (Estimate units)) (Qty units) (Range units)
+
+sum :: List (Estimate units) -> Estimate units
+sum [] = exact Qty.zero
+sum (NonEmpty estimates) = Estimate (sumOf estimates)
+
+sumOf :: NonEmpty (Estimate units) -> Sum units
+sumOf estimates =
+  let individualBounds = NonEmpty.map bounds estimates
+      maxWidth = NonEmpty.maximum (NonEmpty.map Range.width individualBounds)
+      boundsSum = NonEmpty.sum individualBounds
+   in Sum estimates maxWidth boundsSum
+
+instance IsEstimate (Sum units) units where
+  boundsImpl (Sum _ _ boundsSum) = boundsSum
+  refineImpl (Sum estimates maxWidth _) =
+    sumOf (NonEmpty.map (refineWiderThan (0.5 * maxWidth)) estimates)
+
+refineWiderThan :: Qty units -> Estimate units -> Estimate units
+refineWiderThan desiredWidth estimate
+  | Range.width (bounds estimate) > desiredWidth = refine estimate
+  | otherwise = estimate
