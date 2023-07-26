1d2e5a61218bdf9abcf14fffb2a951e853e834c0
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-07-25 22:48:38 -0400

    Add Tests.Random.lengthRange

--- test/Tests/Random.hs
+++ test/Tests/Random.hs
@@ -1,5 +1,6 @@
 module Tests.Random
   ( length
+  , lengthRange
   , point2d
   , vectorBox3d
   )
@@ -11,12 +12,16 @@ import OpenSolid
 import Point2d (Point2d (Point2d))
 import Random (Generator)
 import Random qualified
+import Range (Range)
 import Range qualified
 import Units (Meters)
 import VectorBox3d (VectorBox3d (VectorBox3d))
 
 length :: Generator Length
 length = Random.qtyFrom (Length.meters -10.0) (Length.meters 10.0)
+
+lengthRange :: Generator (Range Meters)
+lengthRange = Range.generator length
 
 point2d :: Generator (Point2d (space @ Meters))
 point2d = Random.map2 Point2d length length
