e2158e1316529a68a783b12a67ef632ce9043bde
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-08-04 23:11:51 -0400

    Add tests of Range.[smallest,largest]

--- test/Tests/Range.hs
+++ test/Tests/Range.hs
@@ -1,18 +1,28 @@
 module Tests.Range (tests) where
 
 import Debug qualified
+import Length (Length)
+import List qualified
+import NonEmpty qualified
 import OpenSolid
 import Parameter1d qualified
 import Qty qualified
+import Random (Generator)
+import Random qualified
+import Range (Range)
 import Range qualified
 import Test (Test)
 import Test qualified
 import Tests.Random qualified as Random
+import Text qualified
+import Units (Meters)
 
 tests :: List Test
 tests =
   [ smaller
   , larger
+  , smallest
+  , largest
   ]
 
 smaller :: Test
@@ -52,3 +62,44 @@ larger = Test.check 100 "larger" $ do
     , "largerValue: " ++ Debug.show largerValue
     , "largerRange: " ++ Debug.show largerRange
     ]
+
+valueInRange :: Generator (Length, Range Meters)
+valueInRange = do
+  range <- Range.generator Random.length
+  t <- Parameter1d.generator
+  let value = Range.interpolate range t
+  Random.return (value, range)
+
+smallest :: Test
+smallest = Test.check 1000 "smallest" $ do
+  valuesAndRanges <- Random.nonEmpty 5 valueInRange
+  let (values, ranges) = NonEmpty.unzip2 valuesAndRanges
+  let smallestValue = Qty.smallest values
+  let smallestRange = Range.smallest ranges
+  Test.expect (Range.includes smallestValue smallestRange) []
+
+largest :: Test
+largest = Test.check 1000 "largest" $ do
+  valuesAndRanges <- Random.nonEmpty 5 valueInRange
+  let (values, ranges) = NonEmpty.unzip2 valuesAndRanges
+  let largestValue = Qty.largest values
+  let largestRange = Range.largest ranges
+  Test.expect (Range.includes largestValue largestRange) $
+    let
+      valueStrings =
+        values
+          |> NonEmpty.toList
+          |> List.map (Debug.show >> ("\n  " ++))
+          |> Text.concat
+
+      rangeStrings =
+        ranges
+          |> NonEmpty.toList
+          |> List.map (Debug.show >> ("\n  " ++))
+          |> Text.concat
+     in
+      [ "values:" ++ valueStrings
+      , "ranges:" ++ rangeStrings
+      , "largestValue: " ++ Debug.show largestValue
+      , "largestRange: " ++ Debug.show largestRange
+      ]
