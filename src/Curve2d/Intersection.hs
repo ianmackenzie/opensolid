a8738a180c7ce9f9ac6e5321b52d2e9b05b070b6
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-08-06 12:31:36 -0400

    Add ApproximateEquality for Curve2d.Intersection
    Useful in test code to check actual vs. expected intersections

--- src/Curve2d/Intersection.hs
+++ src/Curve2d/Intersection.hs
@@ -6,6 +6,7 @@ module Curve2d.Intersection
 where
 
 import OpenSolid
+import Units (Unitless)
 
 data Intersection = Intersection
   { u1 :: Float
@@ -20,3 +21,10 @@ data Kind = Crossing | Tangent deriving (Eq, Ord, Show)
 data TangentIntersectionAtDegeneratePoint
   = TangentIntersectionAtDegeneratePoint
   deriving (Eq, Show, ErrorMessage)
+
+instance ApproximateEquality Intersection Intersection Unitless where
+  intersection1 ~= intersection2 =
+    intersection1.u1 ~= intersection2.u1
+      && intersection1.u2 ~= intersection2.u2
+      && intersection1.kind == intersection2.kind
+      && intersection1.sign == intersection2.sign
