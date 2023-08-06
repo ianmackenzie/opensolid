07cf60591ed11f394a669c60c3b2541d6e40d1f9
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-08-06 12:28:38 -0400

    Implement ApproximateEquality for 2/3/4-tuples

--- src/Tolerance.hs
+++ src/Tolerance.hs
@@ -48,6 +48,39 @@ instance ApproximateEquality a b units => ApproximateEquality (Maybe a) (Maybe b
   Just _ ~= Nothing = False
   Nothing ~= Just _ = False
 
+instance
+  ( ApproximateEquality a1 b1 units
+  , ApproximateEquality a2 b2 units'
+  , units ~ units'
+  ) =>
+  ApproximateEquality (a1, a2) (b1, b2) units
+  where
+  (a1, a2) ~= (b1, b2) = a1 ~= b1 && a2 ~= b2
+
+instance
+  ( ApproximateEquality a1 b1 units
+  , ApproximateEquality a2 b2 units'
+  , ApproximateEquality a3 b3 units''
+  , units ~ units'
+  , units ~ units''
+  ) =>
+  ApproximateEquality (a1, a2, a3) (b1, b2, b3) units
+  where
+  (a1, a2, a3) ~= (b1, b2, b3) = a1 ~= b1 && a2 ~= b2 && a3 ~= b3
+
+instance
+  ( ApproximateEquality a1 b1 units
+  , ApproximateEquality a2 b2 units'
+  , ApproximateEquality a3 b3 units''
+  , ApproximateEquality a4 b4 units'''
+  , units ~ units'
+  , units ~ units''
+  , units ~ units'''
+  ) =>
+  ApproximateEquality (a1, a2, a3, a4) (b1, b2, b3, b4) units
+  where
+  (a1, a2, a3, a4) ~= (b1, b2, b3, b4) = a1 ~= b1 && a2 ~= b2 && a3 ~= b3 && a4 ~= b4
+
 (!=) :: (ApproximateEquality a b units, Tolerance units) => a -> b -> Bool
 (!=) first second = not (first ~= second)
 
