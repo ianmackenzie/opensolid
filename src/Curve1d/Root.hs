7e910e6d3e2858044d639ce96c0b9e60158a3870
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-08-05 23:51:33 -0400

    Implement ApproximateEquality for Curve1d.Root

--- src/Curve1d/Root.hs
+++ src/Curve1d/Root.hs
@@ -1,6 +1,7 @@
 module Curve1d.Root (Root (..)) where
 
 import OpenSolid
+import Units (Unitless)
 
 data Root = Root
   { value :: Float
@@ -8,3 +9,9 @@ data Root = Root
   , sign :: Sign
   }
   deriving (Eq, Show)
+
+instance ApproximateEquality Root Root Unitless where
+  root1 ~= root2 =
+    root1.value ~= root2.value
+      && root1.order == root2.order
+      && root1.sign == root2.sign
