ee2fd94e5fbd9459395b136552d94e1b83aeec65
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-08-06 12:32:59 -0400

    Update Curve2d tests to use new-style expectations

--- test/Tests/Curve2d.hs
+++ test/Tests/Curve2d.hs
@@ -9,8 +9,6 @@ import Curve2d qualified
 import Curve2d.Intersection (Intersection (Intersection))
 import Curve2d.Intersection qualified as Intersection
 import Domain (Domain)
-import Expect (Expectation)
-import Expect qualified
 import Length qualified
 import List qualified
 import OpenSolid
@@ -33,12 +31,6 @@ tests =
   , solving
   ]
 
-equalParameterValues :: List Float -> List Float -> Expectation
-equalParameterValues actual expected =
-  Expect.list Expect.approximately expected actual
- where
-  ?tolerance = 1e-9
-
 parameterValues :: Tolerance Meters => Test
 parameterValues = Test.verify "parameterValues" $ do
   let p1 = Point2d.meters 0.0 0.0
@@ -49,12 +41,14 @@ parameterValues = Test.verify "parameterValues" $ do
   let endParameterValues = Curve2d.parameterValues (Point2d.meters 2.0 0.0) testSpline
   let midParameterValues = Curve2d.parameterValues (Point2d.meters 1.0 1.0) testSpline
   let offCurveParameterValues = Curve2d.parameterValues (Point2d.meters 1.0 1.1) testSpline
-  Expect.all
-    [ equalParameterValues startParameterValues [0.0]
-    , equalParameterValues endParameterValues [1.0]
-    , equalParameterValues midParameterValues [0.5]
-    , equalParameterValues offCurveParameterValues []
-    ]
+  let ?tolerance = 1e-12
+   in Test.assert
+        ( startParameterValues ~= [0.0]
+            && endParameterValues ~= [1.0]
+            && midParameterValues ~= [0.5]
+            && offCurveParameterValues == []
+        )
+        []
 
 overlappingSegments ::
   Tolerance units =>
@@ -66,16 +60,6 @@ overlappingSegments curve1 curve2 =
     Ok _ -> Error "Intersection should have failed (and given overlapping segments)"
     Error (Curve2d.CurvesOverlap segments) -> Ok segments
     Error error -> Error (errorMessage error)
-
-expectSegment :: (Domain, Domain, Sign) -> (Domain, Domain, Sign) -> Expectation
-expectSegment (expectedU, expectedV, expectedSign) (actualU, actualV, actualSign) =
-  Expect.all
-    [ Expect.range expectedU actualU
-    , Expect.range expectedV actualV
-    , Expect.equal expectedSign actualSign
-    ]
- where
-  ?tolerance = 1e-12
 
 curveOverlap1 :: Tolerance Meters => Test
 curveOverlap1 = Test.verify "Overlap detection 1" $ do
@@ -92,7 +76,8 @@ curveOverlap1 = Test.verify "Overlap detection 1" $ do
       , Arc2d.SweptAngle (Angle.degrees 180.0)
       ]
   segments <- overlappingSegments arc1 arc2
-  segments |> Expect.list expectSegment [(Range.from 0.0 0.5, Range.from 0.5 1.0, Positive)]
+  let ?tolerance = 1e-12
+   in Test.assert (segments ~= [(Range.from 0.0 0.5, Range.from 0.5 1.0, Positive)]) []
 
 curveOverlap2 :: Tolerance Meters => Test
 curveOverlap2 = Test.verify "Overlap detection 2" $ do
@@ -111,22 +96,12 @@ curveOverlap2 = Test.verify "Overlap detection 2" $ do
       , Arc2d.EndAngle (Angle.degrees 225.0)
       ]
   segments <- overlappingSegments arc1 arc2
-  segments
-    |> Expect.list
-      expectSegment
-      [ (Range.from 0.0 (1 / 4), Range.from 0.0 (1 / 6), Negative)
-      , (Range.from (3 / 4) 1.0, Range.from (5 / 6) 1.0, Negative)
-      ]
-
-expectIntersection :: Intersection -> Intersection -> Expectation
-expectIntersection expected actual
-  | expected.kind /= actual.kind = Expect.fail "Intersection kinds do not match"
-  | expected.sign /= actual.sign = Expect.fail "Intersection signs do not match"
-  | expected.u1 != actual.u1 || expected.u2 != actual.u2 =
-      Expect.fail "Intersection parameter values do not match"
-  | otherwise = Expect.pass
- where
-  ?tolerance = 1e-12
+  let expectedSegments =
+        [ (Range.from 0.0 (1 / 4), Range.from 0.0 (1 / 6), Negative)
+        , (Range.from (3 / 4) 1.0, Range.from (5 / 6) 1.0, Negative)
+        ]
+  let ?tolerance = 1e-12
+   in Test.assert (segments ~= expectedSegments) []
 
 crossingIntersection :: Tolerance Meters => Test
 crossingIntersection = Test.verify "Crossing intersection" $ do
@@ -143,12 +118,12 @@ crossingIntersection = Test.verify "Crossing intersection" $ do
       , Arc2d.SweptAngle (Angle.degrees -180.0)
       ]
   intersections <- Curve2d.intersections arc1 arc2
-  intersections
-    |> Expect.list
-      expectIntersection
-      [ Intersection 0.0 0.0 Intersection.Crossing Positive
-      , Intersection 0.5 0.5 Intersection.Crossing Negative
-      ]
+  let expectedIntersections =
+        [ Intersection 0.0 0.0 Intersection.Crossing Positive
+        , Intersection 0.5 0.5 Intersection.Crossing Negative
+        ]
+  let ?tolerance = 1e-12
+   in Test.assert (intersections ~= expectedIntersections) []
 
 tangentIntersection :: Tolerance Meters => Test
 tangentIntersection = Test.verify "Tangent intersection" $ do
@@ -167,10 +142,8 @@ tangentIntersection = Test.verify "Tangent intersection" $ do
       , Arc2d.EndAngle Qty.zero
       ]
   intersections <- Curve2d.intersections arc1 arc2
-  intersections
-    |> Expect.list
-      expectIntersection
-      [Intersection 0.5 0.5 Intersection.Tangent Positive]
+  let ?tolerance = 1e-12
+   in Test.assert (intersections ~= [Intersection 0.5 0.5 Intersection.Tangent Positive]) []
 
 solving :: Tolerance Meters => Test
 solving = Test.verify "Solving via Curve1d" $ do
@@ -188,4 +161,4 @@ solving = Test.verify "Solving via Curve1d" $ do
           |> List.map Curve1d.Root.value
           |> List.map (Curve2d.pointOn arc)
           |> List.map (Point2d.distanceFrom Point2d.origin)
-  distances |> Expect.list Expect.approximately [desiredDistance, desiredDistance]
+  Test.assert (distances ~= [desiredDistance, desiredDistance]) []
