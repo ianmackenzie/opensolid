46dd56eb15a2894db5668b5091d6e0dfe6533a7e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-08-05 23:55:28 -0400

    Implement ApproximateEquality for List and Maybe

--- src/Tolerance.hs
+++ src/Tolerance.hs
@@ -18,6 +18,20 @@ class ApproximateEquality a b units | a -> units, b -> units where
 instance units ~ units' => ApproximateEquality (Qty units) (Qty units') units where
   x ~= y = Qty.abs (x - y) <= ?tolerance
 
+instance ApproximateEquality a b units => ApproximateEquality (List a) (List b) units where
+  (x : xs) ~= (y : ys)
+    | x ~= y = xs ~= ys
+    | otherwise = False
+  [] ~= [] = True
+  (_ : _) ~= [] = False
+  [] ~= (_ : _) = False
+
+instance ApproximateEquality a b units => ApproximateEquality (Maybe a) (Maybe b) units where
+  Just a ~= Just b = a ~= b
+  Nothing ~= Nothing = True
+  Just _ ~= Nothing = False
+  Nothing ~= Just _ = False
+
 (!=) :: (ApproximateEquality a b units, Tolerance units) => a -> b -> Bool
 (!=) first second = not (first ~= second)
 
