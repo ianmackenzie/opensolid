e04aa521984b32b6c08b0773a42c2caf04b6e0e1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-08-28 17:47:39 -0400

    Tweak formatting

--- src/VectorBox2d.hs
+++ src/VectorBox2d.hs
@@ -47,11 +47,21 @@ instance Generic.Zero (VectorBox2d (space @ units)) where
 instance Negation (VectorBox2d (space @ units)) where
   negate (VectorBox2d x y) = VectorBox2d (negate x) (negate y)
 
-instance Multiplication Sign (VectorBox2d (space @ units)) (VectorBox2d (space @ units)) where
+instance
+  Multiplication
+    Sign
+    (VectorBox2d (space @ units))
+    (VectorBox2d (space @ units))
+  where
   Positive * vectorBox = vectorBox
   Negative * vectorBox = -vectorBox
 
-instance Multiplication (VectorBox2d (space @ units)) Sign (VectorBox2d (space @ units)) where
+instance
+  Multiplication
+    (VectorBox2d (space @ units))
+    Sign
+    (VectorBox2d (space @ units))
+  where
   vectorBox * Positive = vectorBox
   vectorBox * Negative = -vectorBox
 
