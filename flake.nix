c9d3863a297b1c789f22704126cd8ffbcd62cd22
Andrey Kuzmin <unsoundscapes@gmail.com>
2023-09-18 08:47:30 +0200

    Pin nixpkgs revision and ghc version

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # pinned revision from nixpkgs-unstable branch (2023-09-15)
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=46688f8eb5cd6f1298d873d4d2b9cf245e09e88e";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -10,11 +11,12 @@
   outputs = { flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        ghcVer = "ghc946";
         pkgs = import nixpkgs { inherit system; };
         overlay = final: prev: {
           opensolid = final.callCabal2nix "opensolid" ./. { };
         };
-        myHaskellPackages = pkgs.haskellPackages.extend overlay;
+        myHaskellPackages = pkgs.haskell.packages.${ghcVer}.extend overlay;
       in
       {
         apps = rec {
