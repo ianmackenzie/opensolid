50ec75b31550215e90d76a5528aae6201aaba5f5
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-10-03 21:41:01 -0400

    Simplify Template Haskell export code
    Just grab type signature from the function to be exported;
    also refactor Python code to group C API definitions
    with the code that calls them

--- opensolid-ffi/opensolid.py
+++ opensolid-ffi/opensolid.py
@@ -1,6 +1,8 @@
 from ctypes import *
 import platform
 
+global lib
+
 match platform.system():
     case "Darwin":
         lib = cdll.LoadLibrary("libopensolid-ffi.dylib")
@@ -9,35 +11,31 @@ match platform.system():
     case os:
         raise Exception(f"System {os} is not supported")
 
-lib.xy.argtypes = (c_double, c_double)
-lib.xy.restype = c_void_p
-
-lib.xCoordinate.argtypes = (c_void_p,)
-lib.xCoordinate.restype = c_double
-
-lib.yCoordinate.argtypes = (c_void_p,)
-lib.yCoordinate.restype = c_double
-
-lib.freePoint.argtypes = (c_void_p,)
-
 class Point2d:
     def __init__(self, ptr: c_void_p):
         self.ptr = ptr
 
+    lib.opensolid_point2d_xy.argtypes = (c_double, c_double)
+    lib.opensolid_point2d_xy.restype = c_void_p
     @staticmethod
     def xy(x: float, y: float) -> 'Point2d':
-        return Point2d(lib.xy(x, y))
+        return Point2d(lib.opensolid_point2d_xy(x, y))
 
+    lib.opensolid_point2d_x_coordinate.argtypes = (c_void_p,)
+    lib.opensolid_point2d_x_coordinate.restype = c_double
     @property
-    def x(self) -> float:
-        return lib.xCoordinate(self.ptr)
+    def x_coordinate(self) -> float:
+        return lib.opensolid_point2d_x_coordinate(self.ptr)
 
+    lib.opensolid_point2d_y_coordinate.argtypes = (c_void_p,)
+    lib.opensolid_point2d_y_coordinate.restype = c_double
     @property
-    def y(self) -> float:
-        return lib.yCoordinate(self.ptr)
+    def y_coordinate(self) -> float:
+        return lib.opensolid_point2d_y_coordinate(self.ptr)
 
+    lib.opensolid_point2d_free.argtypes = (c_void_p,)
     def __del__(self):
-        lib.freePoint(self.ptr)
+        lib.opensolid_point2d_free(self.ptr)
 
     def __repr__(self):
-        return f"Point2d({self.x}, {self.y})"
+        return f"Point2d({self.x_coordinate}, {self.y_coordinate})"
--- opensolid-ffi/src/OpenSolidFFI.hs
+++ opensolid-ffi/src/OpenSolidFFI.hs
@@ -1,12 +1,12 @@
 module OpenSolidFFI () where
 
+import Data.String (fromString)
 import Foreign.StablePtr (StablePtr, deRefStablePtr, freeStablePtr, newStablePtr)
 import Language.Haskell.TH qualified as TH
-import OpenSolid hiding ((>>=))
+import OpenSolid hiding (fail, fromString, (>>=))
 import Point2d (Point2d)
 import Point2d qualified
-import Text qualified
-import Prelude (return, (>>=))
+import Prelude (fail, return, sequence, (>>=))
 
 data WorldSpace
 
@@ -15,44 +15,28 @@ type WorldCoordinates = WorldSpace @ Unitless
 xy :: Float -> Float -> IO (StablePtr (Point2d WorldCoordinates))
 xy x y = newStablePtr (Point2d.xy x y)
 
--- Generate 'foreign export' declaration for 'xy' using Template Haskell
-$( let floatType = TH.ConT ''Float
-       ioType = TH.ConT ''IO
-       stablePtrType = TH.ConT ''StablePtr
-       point2dType = TH.ConT ''Point2d
-       worldCoordinatesType = TH.ConT ''WorldCoordinates
-
-       functionType :: List TH.Type -> TH.Type -> TH.Type
-       functionType [] returnType = returnType
-       functionType (firstArgumentType : remainingArgumentTypes) returnType =
-        TH.AppT
-          (TH.AppT TH.ArrowT firstArgumentType)
-          (functionType remainingArgumentTypes returnType)
-
-       xyReturnType =
-        TH.AppT ioType $
-          TH.AppT stablePtrType $
-            TH.AppT point2dType worldCoordinatesType
-
-       xyFunctionType = functionType [floatType, floatType] xyReturnType
-    in return [TH.ForeignD (TH.ExportF TH.CCall (Text.toChars "xy") 'xy xyFunctionType)]
- )
-
-foreign export ccall xCoordinate :: StablePtr (Point2d WorldCoordinates) -> IO Float
-
 xCoordinate :: StablePtr (Point2d WorldCoordinates) -> IO Float
 xCoordinate ptr = do
   point <- deRefStablePtr ptr
   return $ Point2d.xCoordinate point
-
-foreign export ccall yCoordinate :: StablePtr (Point2d WorldCoordinates) -> IO Float
 
 yCoordinate :: StablePtr (Point2d WorldCoordinates) -> IO Float
 yCoordinate ptr = do
   point <- deRefStablePtr ptr
   return $ Point2d.yCoordinate point
 
-foreign export ccall freePoint :: StablePtr (Point2d WorldCoordinates) -> IO ()
-
 freePoint :: StablePtr (Point2d WorldCoordinates) -> IO ()
 freePoint = freeStablePtr
+
+-- Generate 'foreign export' declarations using Template Haskell
+$( do
+    let export exportedName function = do
+          TH.VarI _ functionType _ <- TH.reify function
+          return (TH.ForeignD (TH.ExportF TH.CCall exportedName function functionType))
+    sequence
+      [ export "opensolid_point2d_xy" 'xy
+      , export "opensolid_point2d_x_coordinate" 'xCoordinate
+      , export "opensolid_point2d_y_coordinate" 'yCoordinate
+      , export "opensolid_point2d_free" 'freePoint
+      ]
+ )
