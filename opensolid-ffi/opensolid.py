024259303c0d8d917f833b8c803d2d8ae0bc7487
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-10-03 21:44:16 -0400

    Use lists instead of tuples for 'argtypes'
    Matches the ctypes docs, and avoids ugly trailing comma

--- opensolid-ffi/opensolid.py
+++ opensolid-ffi/opensolid.py
@@ -15,25 +15,25 @@ class Point2d:
     def __init__(self, ptr: c_void_p):
         self.ptr = ptr
 
-    lib.opensolid_point2d_xy.argtypes = (c_double, c_double)
+    lib.opensolid_point2d_xy.argtypes = [c_double, c_double]
     lib.opensolid_point2d_xy.restype = c_void_p
     @staticmethod
     def xy(x: float, y: float) -> 'Point2d':
         return Point2d(lib.opensolid_point2d_xy(x, y))
 
-    lib.opensolid_point2d_x_coordinate.argtypes = (c_void_p,)
+    lib.opensolid_point2d_x_coordinate.argtypes = [c_void_p]
     lib.opensolid_point2d_x_coordinate.restype = c_double
     @property
     def x_coordinate(self) -> float:
         return lib.opensolid_point2d_x_coordinate(self.ptr)
 
-    lib.opensolid_point2d_y_coordinate.argtypes = (c_void_p,)
+    lib.opensolid_point2d_y_coordinate.argtypes = [c_void_p]
     lib.opensolid_point2d_y_coordinate.restype = c_double
     @property
     def y_coordinate(self) -> float:
         return lib.opensolid_point2d_y_coordinate(self.ptr)
 
-    lib.opensolid_point2d_free.argtypes = (c_void_p,)
+    lib.opensolid_point2d_free.argtypes = [c_void_p]
     def __del__(self):
         lib.opensolid_point2d_free(self.ptr)
 
