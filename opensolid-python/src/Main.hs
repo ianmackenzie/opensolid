0b1235b638577848eb00177726cd79cb9f683bca
Andrey Kuzmin <unsoundscapes@gmail.com>
2023-10-25 19:27:25 +0200

    Check the exit code

--- opensolid-python/src/Main.hs
+++ opensolid-python/src/Main.hs
@@ -16,6 +16,7 @@ import OpenSolidAPI
   , openSolidAPI
   )
 import PythonAST qualified as PY
+import System.Exit qualified as SE
 import System.IO qualified as SIO
 import System.Process qualified as SP
 import Task qualified
@@ -305,5 +306,7 @@ main =
         (Just stdinHandle, _, _, process) <- Task.fromIO (SP.createProcess ruffCmd)
         Task.fromIO (SIO.hPutStr stdinHandle pythonCode)
         Task.fromIO (SIO.hClose stdinHandle)
-        _ <- Task.fromIO (SP.waitForProcess process)
-        Task.succeed ()
+        ruffExitCode <- Task.fromIO (SP.waitForProcess process)
+        if ruffExitCode == SE.ExitSuccess
+          then Task.succeed ()
+          else fail "Error when running Ruff"
