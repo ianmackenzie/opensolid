8a073b24ad67dba4d917c14bcd702760a784b445
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2023-12-08 22:04:21 -0500

    Implement SetDifference for lists

--- opensolid/src/SetDifference.hs
+++ opensolid/src/SetDifference.hs
@@ -1,4 +1,10 @@
 module SetDifference (SetDifference ((\\))) where
 
+import Basics
+import Data.List qualified
+
 class SetDifference a b c | a b -> c where
   (\\) :: a -> b -> c
+
+instance (Eq a) => SetDifference (List a) (List a) (List a) where
+  (\\) = (Data.List.\\)
