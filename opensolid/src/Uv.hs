e88acef3b10047638656f2227f86cd16abd7ed3b
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-01-01 14:14:06 -0500

    Add Uv.[bisect,cycle]

--- opensolid/src/Uv.hs
+++ opensolid/src/Uv.hs
@@ -6,6 +6,8 @@ module Uv
   , Direction
   , Bounds
   , domain
+  , bisect
+  , cycle
   )
 where
 
@@ -29,3 +31,11 @@ type Direction = Direction2d Space
 
 domain :: Bounds
 domain = Bounds2d (Range.from 0.0 1.0) (Range.from 0.0 1.0)
+
+bisect :: Parameter -> Bounds -> (Bounds, Bounds)
+bisect U (Bounds2d u v) = let (u1, u2) = Range.bisect u in (Bounds2d u1 v, Bounds2d u2 v)
+bisect V (Bounds2d u v) = let (v1, v2) = Range.bisect v in (Bounds2d u v1, Bounds2d u v2)
+
+cycle :: Parameter -> Parameter
+cycle U = V
+cycle V = U
