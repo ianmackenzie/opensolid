31176c52473df6003ac59ead3e4746344f839c4c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-03-08 16:48:46 -0500

    Add BezierCurve2d.extrapolant

--- opensolid/src/BezierCurve2d.hs
+++ opensolid/src/BezierCurve2d.hs
@@ -1,9 +1,14 @@
-module BezierCurve2d (fromControlPoints) where
+module BezierCurve2d
+  ( fromControlPoints
+  , extrapolant
+  )
+where
 
 import Bounds2d qualified
 import Curve2d (Curve2d)
 import Curve2d qualified
 import Float qualified
+import Int qualified
 import List qualified
 import NonEmpty qualified
 import OpenSolid
@@ -11,6 +16,7 @@ import Point2d (Point2d)
 import Point2d qualified
 import Range (Range (Range))
 import Vector2d (Vector2d)
+import Vector2d qualified
 import VectorCurve2d qualified
 
 newtype BezierCurve2d (coordinateSystem :: CoordinateSystem)
@@ -91,3 +97,57 @@ fromControlPoints ::
   NonEmpty (Point2d (space @ units)) ->
   Result Curve2d.DegenerateCurve (Curve2d (space @ units))
 fromControlPoints controlPoints = Curve2d.from (BezierCurve2d controlPoints)
+
+{- | Construct a Bezier curve with the given start point, start derivatives and end point.
+For example,
+
+> BezierCurve2d.extrapolant p1 [] p2
+
+will result in a straight line (a linear spline) from @p1@ to @p2@, while
+
+> BezierCurve2d.extrapolant p1 [ v1, v2 ] p2
+
+will result in a cubic spline with:
+
+  * Start point @p1@
+  * First derivative at @p1@ equal to @v1@
+  * Second derivative at @p1@ equal to @v2@
+  * End point @p2@
+-}
+extrapolant ::
+  Tolerance units =>
+  Point2d (space @ units) ->
+  List (Vector2d (space @ units)) ->
+  Point2d (space @ units) ->
+  Result Curve2d.DegenerateCurve (Curve2d (space @ units))
+extrapolant startPoint startDerivatives endPoint = do
+  let n = 1 + List.length startDerivatives
+  let qs = scaledDerivatives 1.0 (Float.fromInt n) startDerivatives
+  let controlPoints = startPoint :| followingControlPoints startPoint 1 n qs endPoint
+  fromControlPoints controlPoints
+
+scaledDerivatives :: Float -> Float -> List (Vector2d (space @ units)) -> List (Vector2d (space @ units))
+scaledDerivatives _ _ [] = []
+scaledDerivatives s n (first : rest) = do
+  let s' = s / n
+  let n' = n - 1.0
+  s' * first : scaledDerivatives s' n' rest
+
+offset :: Int -> List (Vector2d (space @ units)) -> Vector2d (space @ units)
+offset i qs =
+  List.take i qs
+    |> List.mapWithIndex (\j q -> Float.fromInt (Int.choose (i - 1) j) * q)
+    |> Vector2d.sum
+
+followingControlPoints ::
+  Point2d (space @ units) ->
+  Int ->
+  Int ->
+  List (Vector2d (space @ units)) ->
+  Point2d (space @ units) ->
+  List (Point2d (space @ units))
+followingControlPoints previousPoint i n qs endPoint
+  | i < n = do
+      let newPoint = previousPoint + offset i qs
+      newPoint : followingControlPoints newPoint (i + 1) n qs endPoint
+  | otherwise = [endPoint]
