d3e0faa3c5f82d4cf1cf1a3ccb4de1c58f30aeb1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-03-30 21:45:43 -0400

    Clean up test code slightly

--- opensolid/test/Tests/Float.hs
+++ opensolid/test/Tests/Float.hs
@@ -12,8 +12,8 @@ tests =
 exponentiation :: Test
 exponentiation =
   Test.group "Exponentiation" $
-    [ Test.verify "2.0 ** 3.0" (Test.expect (2.0 ** 3.0 ~= 8.0))
-    , Test.verify "64.0 ** (1.0 / 3.0)" (Test.expect (64.0 ** (1.0 / 3.0) ~= 4.0))
+    [ Test.verify "2.0 ** 3" (Test.expect (2.0 ** 3 ~= 8.0))
+    , Test.verify "64.0 ** (1 / 3)" (Test.expect (64.0 ** (1 / 3) ~= 4.0))
     ]
  where
   ?tolerance = 1e-12
