4674d84c37243edf90e0673a9e708a4300b0fc34
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-03-30 23:36:14 -0400

    Tweak style

--- opensolid/test/Tests/Direction2d.hs
+++ opensolid/test/Tests/Direction2d.hs
@@ -13,16 +13,15 @@ tests =
   ]
 
 angleFrom :: Test
-angleFrom =
+angleFrom = do
+  let testCase startDegrees endDegrees expectedDegrees =
+        Test.verify "angleFrom" do
+          let startDirection = Direction2d.degrees startDegrees
+          let endDirection = Direction2d.degrees endDegrees
+          let computedAngle = Direction2d.angleFrom startDirection endDirection
+          let expectedAngle = Angle.degrees expectedDegrees
+          Test.expect (Tolerance.using (Angle.radians 1e-12) (computedAngle ~= expectedAngle))
   Test.group "angleFrom" $
     [ testCase 10.0 30.0 20.0
     , testCase 10.0 350.0 -20.0
     ]
- where
-  testCase startDegrees endDegrees expectedDegrees =
-    Test.verify "angleFrom" do
-      let startDirection = Direction2d.degrees startDegrees
-      let endDirection = Direction2d.degrees endDegrees
-      let computedAngle = Direction2d.angleFrom startDirection endDirection
-      let expectedAngle = Angle.degrees expectedDegrees
-      Test.expect (Tolerance.using (Angle.radians 1e-12) (computedAngle ~= expectedAngle))
