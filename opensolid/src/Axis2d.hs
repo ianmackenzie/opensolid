45bc5e6219da1fd7eac9bdb2240579f4eabc1cdb
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-04-13 12:06:03 -0400

    Refactor Axis2d type
    - Hide constructor in favour of using constructor functions
    - Make type parameter phantom to allow units coercion

--- opensolid/src/Axis2d.hs
+++ opensolid/src/Axis2d.hs
@@ -1,11 +1,14 @@
 module Axis2d
-  ( Axis2d (Axis2d, originPoint, direction)
+  ( Axis2d
+  , originPoint
+  , direction
   , x
   , y
   , through
   )
 where
 
+import CoordinateSystem (Space)
 import Direction2d (Direction2d)
 import Direction2d qualified
 import OpenSolid
@@ -13,14 +16,13 @@ import Point2d (Point2d)
 import Point2d qualified
 import Transform2d (Transformable2d (..))
 
-type role Axis2d nominal
+type role Axis2d phantom
 
 data Axis2d (coordinateSystem :: CoordinateSystem) where
   Axis2d ::
-    { originPoint :: Point2d (space @ units)
-    , direction :: Direction2d space
-    } ->
-    Axis2d (space @ units)
+    Point2d coordinateSystem ->
+    Direction2d (Space coordinateSystem) ->
+    Axis2d coordinateSystem
 
 deriving instance Eq (Axis2d (space @ units))
 
@@ -29,15 +31,20 @@ deriving instance Show (Axis2d (space @ units))
 instance (space ~ space', units ~ units') => Transformable2d (Axis2d (space @ units)) (space' @ units') where
   transformBy transformation axis =
     Axis2d
-      { originPoint = transformBy transformation (originPoint axis)
-      , direction = transformBy transformation (direction axis)
-      }
+      (transformBy transformation (originPoint axis))
+      (transformBy transformation (direction axis))
+
+originPoint :: Axis2d (space @ units) -> Point2d (space @ units)
+originPoint (Axis2d p0 _) = p0
+
+direction :: Axis2d (space @ units) -> Direction2d space
+direction (Axis2d _ d) = d
 
 x :: Axis2d (space @ units)
-x = Axis2d{originPoint = Point2d.origin, direction = Direction2d.x}
+x = Axis2d Point2d.origin Direction2d.x
 
 y :: Axis2d (space @ units)
-y = Axis2d{originPoint = Point2d.origin, direction = Direction2d.y}
+y = Axis2d Point2d.origin Direction2d.y
 
 through :: Point2d (space @ units) -> Direction2d space -> Axis2d (space @ units)
 through = Axis2d
