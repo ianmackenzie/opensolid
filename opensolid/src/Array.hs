ed0ee7770a865d0a1b9b9ed6a8d2a88d49170238
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-05-04 10:32:27 -0400

    Make Array a type alias instead of a newtype
    Easier interop with rest of Haskell ecosystem

--- opensolid/src/Array.hs
+++ opensolid/src/Array.hs
@@ -19,37 +19,37 @@ import List qualified
 import OpenSolid
 import Prelude qualified
 
-newtype Array a = Array (Data.Array.Array Int a)
+type Array a = Data.Array.Array Int a
 
 length :: Array a -> Int
-length (Array array) = do
+length array = do
   let (_, i) = Data.Array.bounds array
   i + 1
 
 empty :: Array a
-empty = Array (Data.Array.array (0, -1) [])
+empty = Data.Array.array (0, -1) []
 
 initialize :: Int -> (Int -> a) -> Array a
-initialize n function = Array (Data.Array.array (0, n - 1) [(i, function i) | i <- [0 .. n - 1]])
+initialize n function = Data.Array.array (0, n - 1) [(i, function i) | i <- [0 .. n - 1]]
 
 fromList :: List a -> Array a
-fromList items = Array (Data.Array.listArray (0, List.length items - 1) items)
+fromList items = Data.Array.listArray (0, List.length items - 1) items
 
 toList :: Array a -> List a
-toList (Array array) = Data.Array.elems array
+toList = Data.Array.elems
 
 get :: Int -> Array a -> Maybe a
-get index (Array array) = do
+get index array = do
   let (_, i) = Data.Array.bounds array
   if index >= 0 && index <= i
     then Just (array ! index)
     else Nothing
 
 map :: (a -> b) -> Array a -> Array b
-map function (Array array) = Array (Prelude.fmap function array)
+map = Prelude.fmap
 
 foldl :: (b -> a -> b) -> b -> Array a -> b
-foldl function init (Array array) = Data.Foldable.foldl' function init array
+foldl = Data.Foldable.foldl'
 
 foldr :: (a -> b -> b) -> b -> Array a -> b
-foldr function init (Array array) = Data.Foldable.foldr function init array
+foldr = Data.Foldable.foldr
