e620d460d11785cca61aa25970fc0359fdcc65d4
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-05-18 16:22:32 -0400

    Add Parameter.intervals function

--- opensolid/src/Parameter.hs
+++ opensolid/src/Parameter.hs
@@ -5,6 +5,7 @@ module Parameter
   , trailing
   , inBetween
   , midpoints
+  , intervals
   )
 where
 
@@ -12,6 +13,8 @@ import Float qualified
 import List qualified
 import OpenSolid
 import Random qualified
+import Range (Range)
+import Range qualified
 
 steps :: Int -> List Float
 steps n = if n > 0 then List.map (/ n) [0 .. n] else []
@@ -27,6 +30,12 @@ inBetween n = List.map (/ n) [1 .. n - 1]
 
 midpoints :: Int -> List Float
 midpoints n = List.map (\i -> (2 * i + 1) / (2 * n)) [0 .. n - 1]
+
+intervalOf :: Int -> Int -> Range Unitless
+intervalOf n i = Range.from (i / n) ((i + 1) / n)
+
+intervals :: Int -> List (Range Unitless)
+intervals n = if n > 0 then List.map (intervalOf n) [0 .. n - 1] else []
 
 random :: Random.Generator Float
 random = Float.random 0.0 1.0
