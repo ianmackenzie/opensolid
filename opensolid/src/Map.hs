be5a8c6b0a0d2e76f24e14ce4000387777adc63d
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-05-24 22:27:44 -0400

    Switch to strict Map type by default
    Seems to be recommended in most cases

--- opensolid/src/Map.hs
+++ opensolid/src/Map.hs
@@ -31,94 +31,94 @@ module Map
 where
 
 import Basics
-import Data.Map (Map)
-import Data.Map qualified
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified
 import Pair qualified
 import Prelude qualified
 
 empty :: Map k v
-empty = Data.Map.empty
+empty = Data.Map.Strict.empty
 
 isEmpty :: Map k v -> Bool
-isEmpty = Data.Map.null
+isEmpty = Data.Map.Strict.null
 
 size :: Map k v -> Int
-size = Data.Map.size
+size = Data.Map.Strict.size
 
 singleton :: k -> v -> Map k v
-singleton = Data.Map.singleton
+singleton = Data.Map.Strict.singleton
 
 fromList :: Ord k => List (k, v) -> Map k v
-fromList = Data.Map.fromList
+fromList = Data.Map.Strict.fromList
 
 toList :: Map k v -> List (k, v)
-toList = Data.Map.toList
+toList = Data.Map.Strict.toList
 
 get :: Ord k => k -> Map k v -> Maybe v
-get = Data.Map.lookup
+get = Data.Map.Strict.lookup
 
 take :: Ord k => k -> Map k v -> (Maybe v, Map k v)
-take = Data.Map.updateLookupWithKey (\_ _ -> Nothing)
+take = Data.Map.Strict.updateLookupWithKey (\_ _ -> Nothing)
 
 set :: Ord k => k -> v -> Map k v -> Map k v
-set = Data.Map.insert
+set = Data.Map.Strict.insert
 
 update :: Ord k => (Maybe v -> Maybe v) -> k -> Map k v -> Map k v
-update = Data.Map.alter
+update = Data.Map.Strict.alter
 
 remove :: Ord k => k -> Map k v -> Map k v
-remove = Data.Map.delete
+remove = Data.Map.Strict.delete
 
 keys :: Map k v -> List k
-keys = Data.Map.keys
+keys = Data.Map.Strict.keys
 
 values :: Map k v -> List v
-values = Data.Map.elems
+values = Data.Map.Strict.elems
 
 filter :: (v -> Bool) -> Map k v -> Map k v
-filter = Data.Map.filter
+filter = Data.Map.Strict.filter
 
-filterWithKey :: (k -> v -> Bool) -> Data.Map.Map k v -> Data.Map.Map k v
-filterWithKey = Data.Map.filterWithKey
+filterWithKey :: (k -> v -> Bool) -> Map k v -> Map k v
+filterWithKey = Data.Map.Strict.filterWithKey
 
 map :: (v -> w) -> Map k v -> Map k w
 map = Prelude.fmap
 
 mapWithKey :: (k -> v -> w) -> Map k v -> Map k w
-mapWithKey = Data.Map.mapWithKey
+mapWithKey = Data.Map.Strict.mapWithKey
 
 merge :: Ord k => (v -> v -> v) -> Map k v -> Map k v -> Map k v
-merge = Data.Map.unionWith
+merge = Data.Map.Strict.unionWith
 
 mergeWithKey :: Ord k => (k -> v -> v -> v) -> Map k v -> Map k v -> Map k v
-mergeWithKey = Data.Map.unionWithKey
+mergeWithKey = Data.Map.Strict.unionWithKey
 
 getMin :: Ord k => Map k v -> Maybe (k, v)
-getMin givenMap = if isEmpty givenMap then Nothing else Just (Data.Map.findMin givenMap)
+getMin givenMap = if isEmpty givenMap then Nothing else Just (Data.Map.Strict.findMin givenMap)
 
 getMax :: Ord k => Map k v -> Maybe (k, v)
-getMax givenMap = if isEmpty givenMap then Nothing else Just (Data.Map.findMax givenMap)
+getMax givenMap = if isEmpty givenMap then Nothing else Just (Data.Map.Strict.findMax givenMap)
 
 takeMin :: Ord k => Map k v -> (Maybe (k, v), Map k v)
 takeMin givenMap =
   if isEmpty givenMap
     then (Nothing, givenMap)
-    else Pair.mapFirst Just (Data.Map.deleteFindMin givenMap)
+    else Pair.mapFirst Just (Data.Map.Strict.deleteFindMin givenMap)
 
 takeMax :: Ord k => Map k v -> (Maybe (k, v), Map k v)
 takeMax givenMap =
   if isEmpty givenMap
     then (Nothing, givenMap)
-    else Pair.mapFirst Just (Data.Map.deleteFindMax givenMap)
+    else Pair.mapFirst Just (Data.Map.Strict.deleteFindMax givenMap)
 
 foldr :: (v -> a -> a) -> a -> Map k v -> a
-foldr = Data.Map.foldr
+foldr = Data.Map.Strict.foldr
 
 foldrWithKey :: (k -> v -> a -> a) -> a -> Map k v -> a
-foldrWithKey = Data.Map.foldrWithKey
+foldrWithKey = Data.Map.Strict.foldrWithKey
 
 foldl :: (a -> v -> a) -> a -> Map k v -> a
-foldl = Data.Map.foldl'
+foldl = Data.Map.Strict.foldl'
 
 foldlWithKey :: (a -> k -> v -> a) -> a -> Map k v -> a
-foldlWithKey = Data.Map.foldlWithKey
+foldlWithKey = Data.Map.Strict.foldlWithKey
