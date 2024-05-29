a9c6ee0ac99ae0d70727143d2a78a5cc9f63e7ed
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-05-28 22:06:58 -0400

    Make server example slightly less trivial

--- test-server/src/Main.hs
+++ test-server/src/Main.hs
@@ -1,13 +1,23 @@
 module Main (main) where
 
+import Http.Server (Request, Response, ResponseReceived)
 import Http.Server qualified as Server
 import Http.Server.Request qualified as Request
 import Http.Server.Response qualified as Response
 import OpenSolid
 
+sayHello :: Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+sayHello request respond = respond $
+  case Request.parameterValues "name" request of
+    [name] -> Response.ok [] (Response.text ("Hello, " + name + "!"))
+    [] -> Response.badRequest "Expected a 'name' query parameter"
+    _ -> Response.badRequest "Got multiple 'name' query parameters"
+
 main :: IO ()
 main =
   Server.runOnPort 8000 $
-    \request respond ->
-      case Request.method request of
-        method -> respond (Response.badRequest ("Unsupported method " + method))
+    \request respond -> case Request.method request of
+      "GET" -> case Request.path request of
+        ["hello"] -> sayHello request respond
+        _ -> respond Response.notFound
+      _ -> respond Response.methodNotAllowed
