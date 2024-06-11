1dea138139a4bb14ba27ab127b1ad0b5ac68612c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-06-11 00:48:43 -0400

    Try to make flake.nix a bit more readable

--- flake.nix
+++ flake.nix
@@ -5,23 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
+  # The standard flake-utils boilerplate
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
+        # Actually set up a development shell
+        # with all necessary packages installed
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            zlib
-            ghc
-            cabal-install
-            stack
-            haskell-language-server
-            haskellPackages.fourmolu # For formatting Haskell files
-            haskellPackages.cabal-gild # For formatting Cabal files
-            haskellPackages.implicit-hie # For generating hie.yaml
-            nixpkgs-fmt # For formatting Nix files
-            python312
-            ruff # For formatting/linting Python files
+          buildInputs = [
+            pkgs.ghc # The Haskell compiler itself
+            pkgs.stack # The Stack build tool
+            pkgs.haskell-language-server # Haskell IDE support
+            pkgs.zlib # Needed so that GHC can link against it
+            pkgs.haskellPackages.fourmolu # For formatting Haskell files
+            pkgs.haskellPackages.cabal-gild # For formatting Cabal files
+            pkgs.haskellPackages.implicit-hie # For generating hie.yaml
+            pkgs.nixpkgs-fmt # For formatting Nix files
+            pkgs.python312 # For testing the Python extension
+            pkgs.ruff # For formatting/linting Python files
           ];
         };
       });
