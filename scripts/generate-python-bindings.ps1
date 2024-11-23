d78d02a97cdebad21ac02b33fcd8cc4534a5a6b7
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-11-23 16:44:24 -0500

    Add PowerShell version of generate-python-bindings script

---
+++ scripts/generate-python-bindings.ps1
@@ -0,0 +1,12 @@
+# Run the Python code generator executable to create __init__.py
+cabal run opensolid-python
+# Generate the opensolid-ffi.def file with all API functions listed
+# (necessary for Windows builds)
+cabal run generate-def-file
+# Build the native shared library
+# (on Windows, this uses the opensolid-ffi.def file generated above)
+cabal build opensolid-ffi
+# Copy the native shared library to beside __init__.py
+# so that it can be loaded properly
+Copy-Item -Path .\opensolid-jit\target\release\opensolid_jit.dll -Destination .\opensolid-python\opensolid\
+Get-ChildItem -Path .\dist-newstyle -Filter opensolid-ffi.dll -Recurse | Copy-Item -Destination .\opensolid-python\opensolid\
