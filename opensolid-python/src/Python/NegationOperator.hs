059b4c30a07ce053f1e2c7a050b05d3389114589
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-12-04 16:28:26 -0500

    Tweak codes tyle for consistency

--- opensolid-python/src/Python/NegationOperator.hs
+++ opensolid-python/src/Python/NegationOperator.hs
@@ -5,17 +5,18 @@ import OpenSolid.API.NegationOperator qualified as NegationOperator
 import OpenSolid.FFI (FFI)
 import OpenSolid.FFI qualified as FFI
 import Python qualified
-import Python.Class qualified
 import Python.FFI qualified
+import Python.Type qualified
 
 definition :: forall value. FFI value => FFI.Id value -> Maybe (value -> value) -> Text
 definition classId maybeFunction = case maybeFunction of
   Nothing -> ""
   Just function -> do
     let valueType = NegationOperator.valueType function
+    let valueTypeName = Python.Type.qualifiedName valueType
     let ffiFunctionName = NegationOperator.ffiName classId
     Python.lines
-      [ "def __neg__(self) -> " + Python.Class.unqualifiedName classId + ":"
+      [ "def __neg__(self) -> " + valueTypeName + ":"
       , Python.indent
           [ "output = " + Python.FFI.dummyValue valueType
           , Python.FFI.invoke ffiFunctionName "ctypes.byref(self.__ptr__)" "ctypes.byref(output)"
