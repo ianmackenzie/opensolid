3cde52a1b42105d31878e9b45695d37c9005675f
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-12-28 17:04:18 -0500

    Add Array.[interpolate,aggregate]
    Useful when using arrays as piecewise representations

--- opensolid-core/src/OpenSolid/Array.hs
+++ opensolid-core/src/OpenSolid/Array.hs
@@ -7,6 +7,8 @@ module OpenSolid.Array
   , get
   , first
   , last
+  , interpolate
+  , aggregate
   , map
   , reverse
   , reverseMap
@@ -18,8 +20,13 @@ where
 import Data.Array ((!))
 import Data.Array qualified
 import Data.Foldable qualified
+import OpenSolid.Bounds qualified as Bounds
+import OpenSolid.Float qualified as Float
+import OpenSolid.Int qualified as Int
 import OpenSolid.NonEmpty qualified as NonEmpty
 import OpenSolid.Prelude
+import {-# SOURCE #-} OpenSolid.Range (Range)
+import {-# SOURCE #-} OpenSolid.Range qualified as Range
 import Prelude qualified
 
 data Array a = Array Int (Data.Array.Array Int a) deriving (Show)
@@ -72,3 +79,36 @@ foldl f init (Array _ array) = Data.Foldable.foldl' f init array
 
 foldr :: (a -> b -> b) -> b -> Array a -> b
 foldr f init (Array _ array) = Data.Foldable.foldr f init array
+
+{-# INLINEABLE position #-}
+position :: Int -> Float -> (# Int, Float #)
+position n t = do
+  let n' = Float.int n
+  let i = Int.clampTo (0, n - 1) (Float.floor (t * n'))
+  let t0 = Float.int i / n'
+  (# i, (t - t0) * n' #)
+
+{-# INLINEABLE interpolate #-}
+interpolate :: (a -> Float -> b) -> Array a -> Float -> b
+interpolate f (Array n array) t = do
+  let (# i, ti #) = position n t
+  f (array ! i) ti
+
+aggregate :: Bounds.Interface b => (a -> Range Unitless -> b) -> Array a -> Range Unitless -> b
+aggregate f array t = do
+  let n = length array
+  let (# i, ti #) = position n (Range.lowerBound t)
+  let (# j, tj #) = position n (Range.upperBound t)
+  if i == j
+    then f (get i array) (Range.from ti tj)
+    else do
+      let bi = f (get i array) (Range.from ti 1.0)
+      let bj = f (get j array) (Range.from 0.0 tj)
+      aggregateImpl f array (i + 1) (j - 1) (Bounds.aggregate2 bi bj)
+
+aggregateImpl :: Bounds.Interface b => (a -> Range Unitless -> b) -> Array a -> Int -> Int -> b -> b
+aggregateImpl f array i j acc
+  | i <= j = do
+      let bi = f (get i array) Range.unit
+      aggregateImpl f array (i + 1) j (Bounds.aggregate2 acc bi)
+  | otherwise = acc
--- opensolid-core/src/OpenSolid/Range.hs-boot
+++ opensolid-core/src/OpenSolid/Range.hs-boot
@@ -1,6 +1,7 @@
 module OpenSolid.Range
   ( Range
   , from
+  , unit
   , lowerBound
   , upperBound
   )
@@ -8,6 +9,7 @@ where
 
 import OpenSolid.Bootstrap
 import {-# SOURCE #-} OpenSolid.Qty (Qty)
+import OpenSolid.Unitless (Unitless)
 
 type role Range phantom
 
@@ -15,5 +17,6 @@ type Range :: Type -> Type
 data Range units
 
 from :: Qty units -> Qty units -> Range units
+unit :: Range Unitless
 lowerBound :: Range units -> Qty units
 upperBound :: Range units -> Qty units
