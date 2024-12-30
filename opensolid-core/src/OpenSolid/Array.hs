e6fe01bc68c31c04abfd36bd42f424a115a47fd9
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2024-12-30 11:20:19 -0500

    Add some additional Array mapping functions

--- opensolid-core/src/OpenSolid/Array.hs
+++ opensolid-core/src/OpenSolid/Array.hs
@@ -8,6 +8,9 @@ module OpenSolid.Array
   , first
   , last
   , map
+  , map2
+  , map3
+  , mapWithIndex
   , reverse
   , reverseMap
   , foldl
@@ -18,6 +21,8 @@ where
 import Data.Array ((!))
 import Data.Array qualified
 import Data.Foldable qualified
+import OpenSolid.Int qualified as Int
+import OpenSolid.List qualified as List
 import OpenSolid.NonEmpty qualified as NonEmpty
 import OpenSolid.Prelude
 import Prelude qualified
@@ -54,6 +59,23 @@ last (Array n array) = array ! (n - 1)
 
 map :: (a -> b) -> Array a -> Array b
 map f (Array n array) = Array n (Prelude.fmap f array)
+
+map2 :: (a -> b -> c) -> Array a -> Array b -> Array c
+map2 f (Array n1 array1) (Array n2 array2) = do
+  let newItem i = f (array1 ! i) (array2 ! i)
+  let n = Int.min n1 n2
+  Array n (Data.Array.listArray (0, n - 1) (List.map newItem [0 .. n - 1]))
+
+map3 :: (a -> b -> c -> d) -> Array a -> Array b -> Array c -> Array d
+map3 f (Array n1 array1) (Array n2 array2) (Array n3 array3) = do
+  let newItem i = f (array1 ! i) (array2 ! i) (array3 ! i)
+  let n = Int.min (Int.min n1 n2) n3
+  Array n (Data.Array.listArray (0, n - 1) (List.map newItem [0 .. n - 1]))
+
+mapWithIndex :: (Int -> a -> b) -> Array a -> Array b
+mapWithIndex f (Array n array) = do
+  let newItem i = f i (array ! i)
+  Array n (Data.Array.listArray (0, n - 1) (List.map newItem [0 .. n - 1]))
 
 reverse :: Array a -> Array a
 reverse array = do
