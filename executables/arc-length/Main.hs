a85de57fe771fdb80ea4550b0f7eed16678c0d84
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-01-05 10:07:24 -0500

    Add initial test of arc length parameterization

--- executables/arc-length/Main.hs
+++ executables/arc-length/Main.hs
@@ -1,13 +1,18 @@
 module Main (main) where
 
 import OpenSolid.Area (Area)
+import OpenSolid.Bounds2d qualified as Bounds2d
+import OpenSolid.CubicSpline2d qualified as CubicSpline2d
 import OpenSolid.Curve2d (Curve2d)
 import OpenSolid.Curve2d qualified as Curve2d
+import OpenSolid.Drawing2d qualified as Drawing2d
 import OpenSolid.Float qualified as Float
 import OpenSolid.IO qualified as IO
 import OpenSolid.Length (Length)
 import OpenSolid.Length qualified as Length
 import OpenSolid.Line2d qualified as Line2d
+import OpenSolid.List qualified as List
+import OpenSolid.Parameter qualified as Parameter
 import OpenSolid.Point2d (Point2d)
 import OpenSolid.Point2d qualified as Point2d
 import OpenSolid.Prelude
@@ -29,12 +34,12 @@ testCurve label curve = Tolerance.using (Length.meters 1e-12) IO.do
   (_, length) <- Curve2d.arcLengthParameterization curve
   IO.printLine (label + ": " + formatLength length)
 
-testLine :: IO ()
-testLine = Tolerance.using (Length.meters 1e-6) IO.do
+testLineLength :: IO ()
+testLineLength = Tolerance.using (Length.meters 1e-6) IO.do
   testCurve "Line" (Line2d.from Point2d.origin (Point2d.centimeters 30.0 40.0))
 
-testQuadraticSpline :: IO ()
-testQuadraticSpline = IO.do
+testQuadraticSplineLength :: IO ()
+testQuadraticSplineLength = IO.do
   let p1 = Point2d.origin
   let p2 = Point2d.centimeters 20.0 30.0
   let p3 = Point2d.centimeters 40.0 0.0
@@ -65,7 +70,27 @@ analyticalLength p0 p1 p2 = do
   let ba :: Length = b / a_2
   ((a_32 * s_abc + a_2 * b * (s_abc - c_2) + (4.0 * c * a - b * b) * Float.log ((2.0 * a_2 + ba + s_abc) / (ba + c_2))) / (4.0 * a_32))
 
+testCubicSplineParameterization :: IO ()
+testCubicSplineParameterization = Tolerance.using Length.nanometer IO.do
+  let p1 = Point2d.centimeters 5.0 5.0
+  let p2 = Point2d.centimeters 14.0 15.0
+  let p3 = Point2d.centimeters 16.0 15.0
+  let p4 = Point2d.centimeters 25.0 5.0
+  let spline = CubicSpline2d.fromControlPoints p1 p2 p3 p4
+  (parameterized, length) <- Curve2d.parameterizeByArcLength spline
+  IO.printLine ("Cubic spline: " + formatLength length)
+  let drawCurve fileName curve = IO.do
+        let curveEntity = Drawing2d.curve [] Length.micrometer curve
+        let pointLocations = List.map (Curve2d.evaluate curve) (Parameter.steps 30)
+        let drawPoint point = Drawing2d.circle [Drawing2d.whiteFill] point (Length.millimeters 1.5)
+        let entities = [curveEntity, Drawing2d.group (List.map drawPoint pointLocations)]
+        let drawingBounds = Bounds2d.hull2 Point2d.origin (Point2d.centimeters 30.0 15.0)
+        Drawing2d.writeTo fileName drawingBounds entities
+  drawCurve "executables/arc-length/cubic-spline.svg" spline
+  drawCurve "executables/arc-length/parameterized-spline.svg" parameterized
+
 main :: IO ()
 main = IO.do
-  testLine
-  testQuadraticSpline
+  testLineLength
+  testQuadraticSplineLength
+  testCubicSplineParameterization
