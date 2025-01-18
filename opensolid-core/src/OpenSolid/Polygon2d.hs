61bcd26da45af9c8a90a5af2180536ec0942ec67
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-01-18 00:43:06 -0500

    Refactor Polygon2d module
    - Only support 'ordinary' polygons without holes, for simplicity/clarity
    - Add Polygon2d.[edges,map,signedArea]

--- opensolid-core/src/OpenSolid/Polygon2d.hs
+++ opensolid-core/src/OpenSolid/Polygon2d.hs
@@ -1,46 +1,41 @@
 module OpenSolid.Polygon2d
-  ( Polygon2d
-  , outerLoop
-  , innerLoops
-  , singleLoop
-  , withHoles
+  ( Polygon2d (Polygon2d)
+  , vertices
+  , edges
+  , signedArea
+  , signedArea'
+  , map
   )
 where
 
+import OpenSolid.LineSegment2d (LineSegment2d (LineSegment2d))
 import OpenSolid.List qualified as List
 import OpenSolid.NonEmpty qualified as NonEmpty
 import OpenSolid.Prelude
 import OpenSolid.Qty qualified as Qty
 import OpenSolid.Triangle2d (Triangle2d (Triangle2d))
 import OpenSolid.Triangle2d qualified as Triangle2d
+import OpenSolid.Units qualified as Units
 import OpenSolid.Vertex2d (Vertex2d)
 
-data Polygon2d vertex = Polygon2d
-  { outerLoop :: NonEmpty vertex
-  , innerLoops :: List (NonEmpty vertex)
-  }
+newtype Polygon2d vertex = Polygon2d {vertices :: NonEmpty vertex}
 
-singleLoop :: Vertex2d vertex (space @ units) => NonEmpty vertex -> Polygon2d vertex
-singleLoop givenOuterLoop = withHoles [] givenOuterLoop
+map :: (a -> b) -> Polygon2d a -> Polygon2d b
+map function (Polygon2d vertices) = Polygon2d (NonEmpty.map function vertices)
 
-withHoles ::
-  Vertex2d vertex (space @ units) =>
-  List (NonEmpty vertex) ->
-  NonEmpty vertex ->
-  Polygon2d vertex
-withHoles givenInnerLoops givenOuterLoop =
-  Polygon2d
-    { outerLoop = makeLoop Positive givenOuterLoop
-    , innerLoops = List.map (makeLoop Negative) givenInnerLoops
-    }
-
-makeLoop :: Vertex2d vertex (space @ units) => Sign -> NonEmpty vertex -> NonEmpty vertex
-makeLoop desiredSign vertices =
-  if Qty.sign (counterclockwiseArea' vertices) == desiredSign
-    then vertices
-    else NonEmpty.reverse vertices
-
-counterclockwiseArea' :: Vertex2d vertex (space @ units) => NonEmpty vertex -> Qty (units :*: units)
-counterclockwiseArea' (v0 :| vs) = do
+signedArea' :: Vertex2d vertex (space @ units) => Polygon2d vertex -> Qty (units :*: units)
+signedArea' (Polygon2d (v0 :| vs)) = do
   let triangleSignedArea' v1 v2 = Triangle2d.signedArea' (Triangle2d v0 v1 v2)
   Qty.sum (List.successive triangleSignedArea' vs)
+
+signedArea :: (Vertex2d vertex (space @ units), Units.Squared units squaredUnits) => Polygon2d vertex -> Qty squaredUnits
+signedArea = Units.specialize . signedArea'
+
+edges :: Polygon2d vertex -> NonEmpty (LineSegment2d vertex)
+edges (Polygon2d (v0 :| vs)) = collectEdges v0 v0 vs
+
+collectEdges :: vertex -> vertex -> List vertex -> NonEmpty (LineSegment2d vertex)
+collectEdges first current remaining = case remaining of
+  [] -> NonEmpty.one (LineSegment2d current first)
+  next : following ->
+    NonEmpty.prepend (LineSegment2d current next) (collectEdges first next following)
