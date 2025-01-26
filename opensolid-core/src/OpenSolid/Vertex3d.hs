c2fcad2cceade23a1a4d4381a5475801651ae281
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-01-25 19:37:44 -0500

    Add Vertex3d module

---
+++ opensolid-core/src/OpenSolid/Vertex3d.hs
@@ -0,0 +1,19 @@
+module OpenSolid.Vertex3d
+  ( Vertex3d (..)
+  , pattern Vertex3d
+  )
+where
+
+import OpenSolid.Point3d (Point3d)
+import OpenSolid.Prelude
+
+class Vertex3d vertex (coordinateSystem :: CoordinateSystem) | vertex -> coordinateSystem where
+  position :: vertex -> Point3d coordinateSystem
+
+instance Vertex3d (Point3d (space @ units)) (space @ units) where
+  position = identity
+
+{-# COMPLETE Vertex3d #-}
+
+pattern Vertex3d :: Vertex3d vertex (space @ units) => Point3d (space @ units) -> vertex
+pattern Vertex3d point <- (position -> point)
