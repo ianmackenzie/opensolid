8b07593e7e850df05efe7f7d6cf6e2a24dc98aa2
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-02-17 12:29:58 -0500

    Make Stl module units-agnostic
    Will probably be used always with length units, but there's no real need to enforce that and the flexibility is useful

--- opensolid-core/src/OpenSolid/Stl.hs
+++ opensolid-core/src/OpenSolid/Stl.hs
@@ -1,17 +1,15 @@
 module OpenSolid.Stl (text) where
 
-import OpenSolid.Length (Length)
 import OpenSolid.List qualified as List
 import OpenSolid.Mesh (Mesh)
 import OpenSolid.Mesh qualified as Mesh
 import OpenSolid.Point3d (Point3d (Point3d))
 import OpenSolid.Prelude
 import OpenSolid.Text qualified as Text
-import OpenSolid.Units (Meters)
 import OpenSolid.Vector3d (Vector3d (Vector3d))
 import OpenSolid.Vector3d qualified as Vector3d
 
-text :: (Length -> Float) -> Mesh (Point3d (space @ Meters)) -> Text
+text :: (Qty units -> Float) -> Mesh (Point3d (space @ units)) -> Text
 text units mesh =
   Text.multiline
     [ "solid"
@@ -20,14 +18,12 @@ text units mesh =
     ]
 
 facet ::
-  (Length -> Float) ->
-  (Point3d (space @ Meters), Point3d (space @ Meters), Point3d (space @ Meters)) ->
+  (Qty units -> Float) ->
+  (Point3d (space @ units), Point3d (space @ units), Point3d (space @ units)) ->
   Text
 facet units (p0, p1, p2) = do
-  let crossProduct = (p1 - p0) >< (p2 - p0)
-  let Vector3d nx ny nz
-        | crossProduct == Vector3d.zero = Vector3d.zero
-        | otherwise = crossProduct / Vector3d.magnitude crossProduct
+  let crossProduct = (p1 - p0) .><. (p2 - p0)
+  let Vector3d nx ny nz = Vector3d.normalize crossProduct
   Text.multiline
     [ "facet normal " + Text.float nx + " " + Text.float ny + " " + Text.float nz
     , "    outer loop"
@@ -38,6 +34,6 @@ facet units (p0, p1, p2) = do
     , "endfacet"
     ]
 
-vertex :: (Length -> Float) -> Point3d (space @ Meters) -> Text
+vertex :: (Qty units -> Float) -> Point3d (space @ units) -> Text
 vertex units (Point3d px py pz) =
   "vertex " + Text.float (units px) + " " + Text.float (units py) + " " + Text.float (units pz)
