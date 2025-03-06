85677b3d1374507e76caf3d52c42d6b6f1252c46
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-03-06 14:29:38 -0500

    Add Expression.Curve2d.placeOn

--- opensolid-core/src/OpenSolid/Expression/Curve2d.hs
+++ opensolid-core/src/OpenSolid/Expression/Curve2d.hs
@@ -7,18 +7,25 @@ module OpenSolid.Expression.Curve2d
   , placeIn
   , relativeTo
   , transformBy
+  , placeOn
   )
 where
 
 import OpenSolid.Expression (Expression)
 import OpenSolid.Expression qualified as Expression
+import OpenSolid.Expression.Curve3d qualified as Expression.Curve3d
 import OpenSolid.Expression.VectorCurve2d qualified as Expression.VectorCurve2d
+import OpenSolid.Expression.VectorCurve3d qualified as Expression.VectorCurve3d
 import OpenSolid.Frame2d (Frame2d)
 import OpenSolid.Frame2d qualified as Frame2d
+import OpenSolid.Plane3d (Plane3d)
+import OpenSolid.Plane3d qualified as Plane3d
 import OpenSolid.Point2d (Point2d)
+import OpenSolid.Point3d (Point3d)
 import OpenSolid.Prelude
 import OpenSolid.Transform2d (Transform2d (Transform2d))
 import OpenSolid.Vector2d qualified as Vector2d
+import OpenSolid.Vector3d qualified as Vector3d
 
 constant :: Point2d (space @ units) -> Expression Float (Point2d (space @ units))
 constant = Expression.constant
@@ -64,6 +71,17 @@ relativeTo frame expression = do
   xy
     (displacement <> Expression.VectorCurve2d.constant i)
     (displacement <> Expression.VectorCurve2d.constant j)
+
+placeOn ::
+  Plane3d (global @ units) (Defines local) ->
+  Expression Float (Point2d (local @ units)) ->
+  Expression Float (Point3d (global @ units))
+placeOn plane expression = do
+  let i = Vector3d.unit (Plane3d.xDirection plane)
+  let j = Vector3d.unit (Plane3d.yDirection plane)
+  Expression.Curve3d.constant (Plane3d.originPoint plane)
+    + xCoordinate expression * Expression.VectorCurve3d.constant i
+    + yCoordinate expression * Expression.VectorCurve3d.constant j
 
 transformBy ::
   Transform2d tag (space @ units) ->
