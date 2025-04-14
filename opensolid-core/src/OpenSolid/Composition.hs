3037162ab0c7f00a460a7f9e79d3039f970470a0
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-04-14 00:32:26 -0400

    Remove :.: and :>>: types
    Not used any more

--- opensolid-core/src/OpenSolid/Composition.hs
+++ opensolid-core/src/OpenSolid/Composition.hs
@@ -1,12 +1,9 @@
 module OpenSolid.Composition
   ( Composition ((>>), (.))
-  , (:>>:) (..)
-  , type (:.:)
-  , pattern (:.:)
   )
 where
 
-import Prelude (Eq, IO, Show, type (~))
+import Prelude (IO, type (~))
 import Prelude qualified
 
 class Composition a b c | a b -> c where
@@ -25,14 +22,3 @@ instance b ~ b' => Composition (a -> b) (b' -> c) (a -> c) where
 
 instance Composition (IO ()) (IO a) (IO a) where
   (>>) = (Prelude.>>)
-
-data a :>>: b = a :>>: b deriving (Eq, Show)
-
-type a :.: b = b :>>: a
-
-{-# COMPLETE (:.:) #-}
-
-pattern (:.:) :: a -> b -> a :.: b
-pattern a :.: b <- b :>>: a
-  where
-    a :.: b = b :>>: a
