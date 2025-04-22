913f3a19d20a9ded6a9965f95e021c0e34b5c8eb
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-04-22 15:09:32 -0400

    Try a new combination for multithreaded FFI: safe foreign calls, enable multiple threads with "-N" RTS option

--- opensolid-ffi/src/wrapper.c
+++ opensolid-ffi/src/wrapper.c
@@ -1,24 +1,33 @@
 #include <stdlib.h>
 
-extern void hs_init(int *argc, char **argv[]);
-extern void hs_exit(void);
+extern void
+hs_init(int* argc, char*** argv);
+extern void
+hs_exit(void);
 
-void opensolid_init()
-{
-  hs_init(NULL, NULL);
+void
+opensolid_init() {
+  int argc = 4;
+  char** argv = malloc(argc * sizeof(char*));
+  argv[0] = "opensolid-ffi";
+  argv[1] = "+RTS";
+  argv[2] = "-N";
+  argv[3] = "-RTS";
+  hs_init(&argc, &argv);
+  free(argv);
 }
 
-void opensolid_exit()
-{
+void
+opensolid_exit() {
   hs_exit();
 }
 
-void* opensolid_malloc(size_t bytes)
-{
+void*
+opensolid_malloc(size_t bytes) {
   return malloc(bytes);
 }
 
-void opensolid_free(void *ptr)
-{
+void
+opensolid_free(void* ptr) {
   free(ptr);
 }
