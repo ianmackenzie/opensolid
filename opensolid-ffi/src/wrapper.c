f6f7e97b00e9b69bb16759d692796d784b10d268
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-04-22 15:09:32 -0400

    Fix / clean up FFI wrapper.c

--- opensolid-ffi/src/wrapper.c
+++ opensolid-ffi/src/wrapper.c
@@ -1,24 +1,31 @@
 #include <stdlib.h>
 
-extern void
-hs_init(int* argc, char*** argv);
-extern void
-hs_exit(void);
+// Path to Rts.h will be set by GHC when compiling
+#include "Rts.h"
 
 void
 opensolid_init() {
+  // Defined in Rts.h
+  RtsConfig rtsConfig = defaultRtsConfig;
+  rtsConfig.rts_opts_enabled = RtsOptsAll;
+
+  // Set up RTS options to enable using multiple OS threads
   int argc = 4;
   char** argv = malloc(argc * sizeof(char*));
   argv[0] = "opensolid-ffi";
   argv[1] = "+RTS";
   argv[2] = "-N";
   argv[3] = "-RTS";
-  hs_init(&argc, &argv);
+
+  // Defined in Rts.h
+  hs_init_ghc(&argc, &argv, rtsConfig);
+
   free(argv);
 }
 
 void
 opensolid_exit() {
+  // Defined in Rts.h
   hs_exit();
 }
 
