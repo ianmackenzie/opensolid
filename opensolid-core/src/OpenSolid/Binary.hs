f031514efcae17ea0b23f16d7d4467ed47ecaf41
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-06-03 22:01:07 -0400

    Add some convenience functions for encoding numeric values as little-endian

--- opensolid-core/src/OpenSolid/Binary.hs
+++ opensolid-core/src/OpenSolid/Binary.hs
@@ -5,6 +5,14 @@ module OpenSolid.Binary
   , empty
   , concat
   , collect
+  , uint8
+  , uint16LE
+  , uint32LE
+  , uint64LE
+  , int16LE
+  , int32LE
+  , int64LE
+  , float64LE
   )
 where
 
@@ -14,6 +22,9 @@ import Data.ByteString.Builder (Builder)
 import Data.ByteString.Builder qualified as Builder
 import OpenSolid.Bootstrap
 import OpenSolid.Composition
+import OpenSolid.Float qualified as Float
+import OpenSolid.Int qualified as Int
+import OpenSolid.Prelude
 import Prelude qualified
 
 bytes :: Builder -> ByteString
@@ -27,3 +38,27 @@ concat = Prelude.mconcat
 
 collect :: Foldable list => (a -> Builder) -> list a -> Builder
 collect = Prelude.foldMap
+
+uint8 :: Int -> Builder
+uint8 = Builder.word8 . Int.toWord8
+
+uint16LE :: Int -> Builder
+uint16LE = Builder.word16LE . Int.toWord16
+
+uint32LE :: Int -> Builder
+uint32LE = Builder.word32LE . Int.toWord32
+
+uint64LE :: Int -> Builder
+uint64LE = Builder.word64LE . Int.toWord64
+
+int16LE :: Int -> Builder
+int16LE = Builder.int16LE . Int.toInt16
+
+int32LE :: Int -> Builder
+int32LE = Builder.int32LE . Int.toInt32
+
+int64LE :: Int -> Builder
+int64LE = Builder.int64LE . Int.toInt64
+
+float64LE :: Float -> Builder
+float64LE = Builder.doubleLE . Float.toDouble
