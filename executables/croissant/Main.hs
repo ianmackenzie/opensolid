42be14a8041dd9c83f3a15552401aeb1fcb9b341
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-06-03 22:01:07 -0400

    Remove one use of rightwardForwardUpward
    Seems a bit more readable and flexible (could e.g. use leftward direction just as easily)

--- executables/croissant/Main.hs
+++ executables/croissant/Main.hs
@@ -4,6 +4,7 @@ import OpenSolid.Angle qualified as Angle
 import OpenSolid.Body3d qualified as Body3d
 import OpenSolid.Convention3d qualified as Convention3d
 import OpenSolid.Curve qualified as Curve
+import OpenSolid.Frame3d qualified as Frame3d
 import OpenSolid.IO qualified as IO
 import OpenSolid.Length qualified as Length
 import OpenSolid.Mesh qualified as Mesh
@@ -13,21 +14,22 @@ import OpenSolid.Region2d qualified as Region2d
 import OpenSolid.Stl qualified as Stl
 import OpenSolid.Surface3d qualified as Surface3d
 import OpenSolid.SurfaceFunction qualified as SurfaceFunction
-import OpenSolid.SurfaceFunction3d qualified as SurfaceFunction3d
 import OpenSolid.Tolerance qualified as Tolerance
 
 main :: IO ()
 main = Tolerance.using Length.nanometer IO.do
+  let world = Frame3d.world
   let majorRadius = Length.meter
   let k = Length.meters 2.0
   let minorRadius = Curve.hermite Length.zero [k] Length.zero [-k] . SurfaceFunction.u
   let theta = Curve.line (Angle.degrees 45.0) (Angle.degrees 315.0) . SurfaceFunction.u
   let phi = Angle.twoPi * SurfaceFunction.v
   let r = majorRadius + minorRadius * SurfaceFunction.cos phi
-  let rightward = r * SurfaceFunction.cos theta
-  let forward = r * SurfaceFunction.sin theta
-  let upward = minorRadius * SurfaceFunction.sin phi
-  let surfaceFunction = SurfaceFunction3d.rightwardForwardUpward rightward forward upward
+  let surfaceFunction =
+        world.originPoint
+          + r * SurfaceFunction.cos theta * world.rightwardDirection
+          + r * SurfaceFunction.sin theta * world.forwardDirection
+          + minorRadius * SurfaceFunction.sin phi * world.upwardDirection
   let surface = Surface3d.parametric surfaceFunction Region2d.unitSquare
   body <- Body3d.boundedBy [surface]
   let constraints = NonEmpty.one (Mesh.maxSize (Length.centimeters 20.0))
