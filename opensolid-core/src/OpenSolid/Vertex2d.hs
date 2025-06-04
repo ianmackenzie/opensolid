fcaa05651fe60e94c25042f7f59c43d4c82ff020
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-06-03 22:01:07 -0400

    Make Eq a superclass of Vertex2d
    Consistent with Vertex3d, allows checking for identical/duplicate vertices

--- opensolid-core/src/OpenSolid/Vertex2d.hs
+++ opensolid-core/src/OpenSolid/Vertex2d.hs
@@ -7,7 +7,11 @@ where
 import OpenSolid.Point2d (Point2d)
 import OpenSolid.Prelude
 
-class Vertex2d vertex (coordinateSystem :: CoordinateSystem) | vertex -> coordinateSystem where
+class
+  Eq vertex =>
+  Vertex2d vertex (coordinateSystem :: CoordinateSystem)
+    | vertex -> coordinateSystem
+  where
   position :: vertex -> Point2d coordinateSystem
 
 instance Vertex2d (Point2d (space @ units)) (space @ units) where
