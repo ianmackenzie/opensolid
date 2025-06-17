5a6607effbd1896083428b2b7f2e73e8aac3e0cb
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-06-16 23:02:59 -0400

    Refactor LineSegment2d
    Add some useful accessors, and move towards a pattern where all fields are simple aliases for an equivalent named function
    (having a named function is sometimes useful to help with type inference, and also makes it possible to extract docstrings easily for target languages...)

--- opensolid-core/src/OpenSolid/LineSegment2d.hs
+++ opensolid-core/src/OpenSolid/LineSegment2d.hs
@@ -1,47 +1,91 @@
 module OpenSolid.LineSegment2d
   ( LineSegment2d (LineSegment2d)
+  , startVertex
+  , endVertex
+  , startPoint
+  , endPoint
+  , length
+  , bounds
   , distanceTo
   )
 where
 
 import OpenSolid.Bounded2d (Bounded2d)
 import OpenSolid.Bounded2d qualified as Bounded2d
+import OpenSolid.Bounds2d (Bounds2d)
 import OpenSolid.Bounds2d qualified as Bounds2d
 import OpenSolid.Point2d (Point2d)
 import OpenSolid.Point2d qualified as Point2d
 import OpenSolid.Prelude
 import OpenSolid.Qty qualified as Qty
 import OpenSolid.Vector2d qualified as Vector2d
-import OpenSolid.Vertex2d (Vertex2d, pattern Vertex2d)
+import OpenSolid.Vertex2d (Vertex2d)
+import OpenSolid.Vertex2d qualified as Vertex2d
 
 data LineSegment2d vertex = LineSegment2d vertex vertex
 
-instance HasField "start" (LineSegment2d vertex) vertex where
-  getField (LineSegment2d start _) = start
+instance HasField "startVertex" (LineSegment2d vertex) vertex where
+  getField = startVertex
 
-instance HasField "end" (LineSegment2d vertex) vertex where
-  getField (LineSegment2d _ end) = end
+instance HasField "endVertex" (LineSegment2d vertex) vertex where
+  getField = endVertex
 
 instance
   Vertex2d vertex (space @ units) =>
-  Bounded2d (LineSegment2d vertex) (space @ units)
+  HasField "startPoint" (LineSegment2d vertex) (Point2d (space @ units))
   where
-  bounds (LineSegment2d (Vertex2d startPoint) (Vertex2d endPoint)) =
-    Bounds2d.hull2 startPoint endPoint
+  getField = startPoint
+
+instance
+  Vertex2d vertex (space @ units) =>
+  HasField "endPoint" (LineSegment2d vertex) (Point2d (space @ units))
+  where
+  getField = endPoint
 
 instance
   Vertex2d vertex (space @ units) =>
   HasField "length" (LineSegment2d vertex) (Qty units)
   where
-  getField (LineSegment2d (Vertex2d startPoint) (Vertex2d endPoint)) =
-    Point2d.distanceFrom startPoint endPoint
+  getField = length
+
+instance
+  Vertex2d vertex (space @ units) =>
+  HasField "bounds" (LineSegment2d vertex) (Bounds2d (space @ units))
+  where
+  getField = bounds
+
+instance
+  Vertex2d vertex (space @ units) =>
+  Bounded2d (LineSegment2d vertex) (space @ units)
+  where
+  bounds = bounds
+
+startVertex :: LineSegment2d vertex -> vertex
+startVertex (LineSegment2d s _) = s
+
+endVertex :: LineSegment2d vertex -> vertex
+endVertex (LineSegment2d _ e) = e
+
+startPoint :: Vertex2d vertex (space @ units) => LineSegment2d vertex -> Point2d (space @ units)
+startPoint = Vertex2d.position . startVertex
+
+endPoint :: Vertex2d vertex (space @ units) => LineSegment2d vertex -> Point2d (space @ units)
+endPoint = Vertex2d.position . endVertex
+
+length :: Vertex2d vertex (space @ units) => LineSegment2d vertex -> Qty units
+length segment = Point2d.distanceFrom segment.startPoint segment.endPoint
+
+bounds :: Vertex2d vertex (space @ units) => LineSegment2d vertex -> Bounds2d (space @ units)
+bounds segment = Bounds2d.hull2 segment.startPoint segment.endPoint
 
 distanceTo ::
   Vertex2d vertex (space @ units) =>
   Point2d (space @ units) ->
   LineSegment2d vertex ->
   Qty units
-distanceTo p0 (LineSegment2d (Vertex2d p1) (Vertex2d p2)) = do
+distanceTo p0 segment = do
+  let p1 = segment.startPoint
+  let p2 = segment.endPoint
   let u = p0 - p1
   let v = p2 - p1
   let dSquared' = Vector2d.squaredMagnitude' v
