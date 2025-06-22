64126ee1bccc6b5fe0afa776dea0ed6529a926f3
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-06-21 22:59:00 -0400

    Add support for tuples of up to 8 named arguments
    Implement composition via do-blocks, plus field access to allow for 'args.someArgName' syntax

--- opensolid-core/src/OpenSolid/Field.hs
+++ opensolid-core/src/OpenSolid/Field.hs
@@ -42,6 +42,72 @@ instance
   where
   field1 >> (field2, field3, field4, field5) = (field1, field2, field3, field4, field5)
 
+instance
+  Composition
+    (name1 ::: value1)
+    ( name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    )
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    )
+  where
+  field1 >> (field2, field3, field4, field5, field6) =
+    (field1, field2, field3, field4, field5, field6)
+
+instance
+  Composition
+    (name1 ::: value1)
+    ( name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+  where
+  field1 >> (field2, field3, field4, field5, field6, field7) =
+    (field1, field2, field3, field4, field5, field6, field7)
+
+instance
+  Composition
+    (name1 ::: value1)
+    ( name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+  where
+  field1 >> (field2, field3, field4, field5, field6, field7, field8) =
+    (field1, field2, field3, field4, field5, field6, field7, field8)
+
 instance HasField name1 (name1 ::: value1, name2 ::: value2) value1 where
   getField (Field value1, _) = value1
 
@@ -169,3 +235,320 @@ instance
     value5
   where
   getField (_, _, _, _, Field value5) = value5
+
+instance
+  HasField
+    name1
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    )
+    value1
+  where
+  getField (Field value1, _, _, _, _, _) = value1
+
+instance
+  HasField
+    name2
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    )
+    value2
+  where
+  getField (_, Field value2, _, _, _, _) = value2
+
+instance
+  HasField
+    name3
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    )
+    value3
+  where
+  getField (_, _, Field value3, _, _, _) = value3
+
+instance
+  HasField
+    name4
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    )
+    value4
+  where
+  getField (_, _, _, Field value4, _, _) = value4
+
+instance
+  HasField
+    name5
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    )
+    value5
+  where
+  getField (_, _, _, _, Field value5, _) = value5
+
+instance
+  HasField
+    name6
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    )
+    value6
+  where
+  getField (_, _, _, _, _, Field value6) = value6
+
+instance
+  HasField
+    name1
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+    value1
+  where
+  getField (Field value1, _, _, _, _, _, _) = value1
+
+instance
+  HasField
+    name2
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+    value2
+  where
+  getField (_, Field value2, _, _, _, _, _) = value2
+
+instance
+  HasField
+    name3
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+    value3
+  where
+  getField (_, _, Field value3, _, _, _, _) = value3
+
+instance
+  HasField
+    name4
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+    value4
+  where
+  getField (_, _, _, Field value4, _, _, _) = value4
+
+instance
+  HasField
+    name5
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+    value5
+  where
+  getField (_, _, _, _, Field value5, _, _) = value5
+
+instance
+  HasField
+    name6
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+    value6
+  where
+  getField (_, _, _, _, _, Field value6, _) = value6
+
+instance
+  HasField
+    name7
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    )
+    value7
+  where
+  getField (_, _, _, _, _, _, Field value7) = value7
+
+instance
+  HasField
+    name1
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    value1
+  where
+  getField (Field value1, _, _, _, _, _, _, _) = value1
+
+instance
+  HasField
+    name2
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    value2
+  where
+  getField (_, Field value2, _, _, _, _, _, _) = value2
+
+instance
+  HasField
+    name3
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    value3
+  where
+  getField (_, _, Field value3, _, _, _, _, _) = value3
+
+instance
+  HasField
+    name4
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    value4
+  where
+  getField (_, _, _, Field value4, _, _, _, _) = value4
+
+instance
+  HasField
+    name5
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    value5
+  where
+  getField (_, _, _, _, Field value5, _, _, _) = value5
+
+instance
+  HasField
+    name6
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    value6
+  where
+  getField (_, _, _, _, _, Field value6, _, _) = value6
+
+instance
+  HasField
+    name7
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    value7
+  where
+  getField (_, _, _, _, _, _, Field value7, _) = value7
+
+instance
+  HasField
+    name8
+    ( name1 ::: value1
+    , name2 ::: value2
+    , name3 ::: value3
+    , name4 ::: value4
+    , name5 ::: value5
+    , name6 ::: value6
+    , name7 ::: value7
+    , name8 ::: value8
+    )
+    value8
+  where
+  getField (_, _, _, _, _, _, _, Field value8) = value8
