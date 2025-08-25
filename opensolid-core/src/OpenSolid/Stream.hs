d102a42f54ee63e1c87f3320c8446f1543f0df80
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-08-24 22:05:49 -0400

    Add some more Stream utility functions

--- opensolid-core/src/OpenSolid/Stream.hs
+++ opensolid-core/src/OpenSolid/Stream.hs
@@ -2,17 +2,21 @@ module OpenSolid.Stream
   ( Stream (..)
   , repeat
   , iterate
+  , from
   , head
   , tail
+  , toList
   , nth
   , drop
   , map
   , mapWithIndex
+  , map2
   , take
   )
 where
 
 import OpenSolid.Prelude
+import Prelude (Enum)
 import Prelude qualified
 
 data Stream a = Stream a ~(Stream a)
@@ -25,6 +29,9 @@ repeat value = Stream value (repeat value)
 iterate :: (a -> a) -> a -> Stream a
 iterate function first = Stream first (iterate function (function first))
 
+from :: Enum a => a -> Stream a
+from value = Stream value (from (Prelude.succ value))
+
 {-# INLINE head #-}
 head :: Stream a -> a
 head (Stream first _) = first
@@ -32,6 +39,9 @@ head (Stream first _) = first
 {-# INLINE tail #-}
 tail :: Stream a -> Stream a
 tail (Stream _ rest) = rest
+
+toList :: Stream a -> List a
+toList (Stream first rest) = first : toList rest
 
 nth :: Int -> Stream a -> a
 nth n stream = if n <= 0 then head stream else nth (n - 1) (tail stream)
@@ -45,6 +55,10 @@ mapWithIndex = mapWithIndexImpl 0
 mapWithIndexImpl :: Int -> (Int -> a -> b) -> Stream a -> Stream b
 mapWithIndexImpl index function (Stream first rest) =
   Stream (function index first) (mapWithIndexImpl (index + 1) function rest)
+
+map2 :: (a -> b -> c) -> Stream a -> Stream b -> Stream c
+map2 f (Stream firstA restA) (Stream firstB restB) =
+  Stream (f firstA firstB) (map2 f restA restB)
 
 take :: Int -> Stream a -> List a
 take n _ | n <= 0 = []
