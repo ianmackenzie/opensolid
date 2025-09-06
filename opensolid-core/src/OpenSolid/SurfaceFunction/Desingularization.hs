e0b55692c2ae9a3c305bc030fc536884b158767e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-09-05 21:00:47 -0400

    Split out test point generation in SurfaceFunction.Desingularization into its own function, and add some module docs
    For square root desingularization it will be necessary to use the test points directly instead of just calling isZero

--- opensolid-core/src/OpenSolid/SurfaceFunction/Desingularization.hs
+++ opensolid-core/src/OpenSolid/SurfaceFunction/Desingularization.hs
@@ -1,4 +1,5 @@
-module OpenSolid.SurfaceFunction.Desingularization (isZero) where
+-- | Helper functions for surface function desingularization.
+module OpenSolid.SurfaceFunction.Desingularization (isZero, testPoints) where
 
 import OpenSolid.List qualified as List
 import OpenSolid.Parameter qualified as Parameter
@@ -8,10 +9,27 @@ import OpenSolid.Qty qualified as Qty
 import {-# SOURCE #-} OpenSolid.SurfaceFunction (SurfaceFunction)
 import {-# SOURCE #-} OpenSolid.SurfaceFunction qualified as SurfaceFunction
 import OpenSolid.SurfaceParameter (SurfaceParameter (U, V))
+import OpenSolid.UvPoint (UvPoint)
 
+{-| Generate a list of test points at a given fixed U or V value, varying the other parameter.
+
+The given value should generally be either 0 or 1;
+for example 'testPoints U 0.0' will generate some test points with different V values along U=0,
+suitable for testing if some function is zero everywhere at U=0.
+(Under the hood this calls 'Parameter.samples' to generate the varying parameter values.)
+-}
+testPoints :: SurfaceParameter -> Float -> List UvPoint
+testPoints U uValue = [Point2d uValue v | v <- Parameter.samples]
+testPoints V vValue = [Point2d u vValue | u <- Parameter.samples]
+
+{-| Check if a given surface function is zero everywhere at a given fixed U or V value.
+
+The given value should generally either be 0 or 1, for example 'isZero U 0.0 function'
+will check if the given function is zero everywhere along U=0.
+-}
 isZero :: Tolerance units => SurfaceParameter -> Float -> SurfaceFunction units -> Bool
-isZero parameter value function = do
-  let uvPoints = case parameter of
-        U -> [Point2d value v | v <- Parameter.samples]
-        V -> [Point2d u value | u <- Parameter.samples]
-  List.allTrue [SurfaceFunction.evaluate function uvPoint ~= Qty.zero | uvPoint <- uvPoints]
+isZero parameter value function =
+  List.allTrue
+    [ SurfaceFunction.evaluate function testPoint ~= Qty.zero
+    | testPoint <- testPoints parameter value
+    ]
