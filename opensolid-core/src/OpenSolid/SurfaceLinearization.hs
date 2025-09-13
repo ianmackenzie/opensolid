ecf7da4026dd997758d2aa62a5d4e35172c0f78e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-09-12 23:18:40 -0400

    Try to optimize SurfaceLinearization.error by delegating to an optimized/unboxed version
    Might not do all that much given that there are still some VectorBounds3d additions/multiplications being done...

--- opensolid-core/src/OpenSolid/SurfaceLinearization.hs
+++ opensolid-core/src/OpenSolid/SurfaceLinearization.hs
@@ -2,9 +2,9 @@ module OpenSolid.SurfaceLinearization (error) where
 
 import OpenSolid.Bounds qualified as Bounds
 import OpenSolid.Bounds2d (Bounds2d (Bounds2d))
-import OpenSolid.Float qualified as Float
 import OpenSolid.Prelude
-import OpenSolid.Qty qualified as Qty
+import OpenSolid.Qty (Qty (Qty#))
+import OpenSolid.Unboxed.Math
 import OpenSolid.UvBounds (UvBounds)
 import OpenSolid.VectorBounds3d (VectorBounds3d)
 import OpenSolid.VectorBounds3d qualified as VectorBounds3d
@@ -15,17 +15,26 @@ error ::
   VectorBounds3d (space @ units) ->
   UvBounds ->
   Qty units
-error fuu fuv fvv subdomain = do
-  let Bounds2d uBounds vBounds = subdomain
-  let uWidthSquared = Float.squared (Bounds.width uBounds)
-  let vWidthSquared = Float.squared (Bounds.width vBounds)
-  let uvWidthSquared = uWidthSquared + vWidthSquared
-  let uuMagnitude = Bounds.maxAbs (VectorBounds3d.magnitude fuu)
-  let vvMagnitude = Bounds.maxAbs (VectorBounds3d.magnitude fvv)
-  let uvMagnitude1 = Bounds.maxAbs (VectorBounds3d.magnitude (fuu + fvv + 2.0 * fuv))
-  let uvMagnitude2 = Bounds.maxAbs (VectorBounds3d.magnitude (fuu + fvv - 2.0 * fuv))
-  let uuError = 0.125 * uuMagnitude * uWidthSquared
-  let vvError = 0.125 * vvMagnitude * vWidthSquared
-  let uvError1 = 0.125 * uvMagnitude1 * uvWidthSquared
-  let uvError2 = 0.125 * uvMagnitude2 * uvWidthSquared
-  Qty.max (Qty.max uuError vvError) (Qty.max uvError1 uvError2)
+error fuu fuv fvv subdomain = Qty# (error# fuu fuv fvv subdomain)
+
+error# ::
+  VectorBounds3d (space @ units) ->
+  VectorBounds3d (space @ units) ->
+  VectorBounds3d (space @ units) ->
+  UvBounds ->
+  Double#
+error# fuu fuv fvv (Bounds2d uBounds vBounds) = do
+  let uWidthSquared# = squared# (Bounds.width# uBounds)
+  let vWidthSquared# = squared# (Bounds.width# vBounds)
+  let uvWidthSquared# = uWidthSquared# +# vWidthSquared#
+  let uuMagnitude# = VectorBounds3d.maxMagnitude# fuu
+  let vvMagnitude# = VectorBounds3d.maxMagnitude# fvv
+  let fA = fuu + fvv
+  let fB = 2.0 * fuv
+  let uvMagnitude1# = VectorBounds3d.maxMagnitude# (fA + fB)
+  let uvMagnitude2# = VectorBounds3d.maxMagnitude# (fA - fB)
+  let uuError# = 0.125## *# uuMagnitude# *# uWidthSquared#
+  let vvError# = 0.125## *# vvMagnitude# *# vWidthSquared#
+  let uvError1# = 0.125## *# uvMagnitude1# *# uvWidthSquared#
+  let uvError2# = 0.125## *# uvMagnitude2# *# uvWidthSquared#
+  max# (max# uuError# vvError#) (max# uvError1# uvError2#)
