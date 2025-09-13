af82fa73b29d906ba7aef0e8b4dc9c6f8d283709
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-09-12 23:18:40 -0400

    Tweak Python wheel building to indicate that wheels are compatible with any Python 3 version
    Should even work on non-CPython implementations, since e.g. Jython and IronPython do both include a ctypes module

--- opensolid-python/lib/setup.py
+++ opensolid-python/lib/setup.py
@@ -4,13 +4,14 @@ from setuptools.command.bdist_wheel import bdist_wheel
 
 class custom_bdist_wheel(bdist_wheel):
     def get_tag(self) -> tuple[str, str, str]:
-        """Override get_tag to set ABI tag to 'none'.
+        """Override get_tag to set implementation tag to 'py3' and ABI tag to 'none'.
 
         The OpenSolid native library is accessed only by ctypes calls,
-        and so is independent of the Python ABI.
+        and so is independent of the Python ABI or even Python version,
+        but is still specific to the platform it was built for.
         """
         impl_tag, abi_tag, plat_tag = bdist_wheel.get_tag(self)
-        return impl_tag, "none", plat_tag
+        return "py3", "none", plat_tag
 
 
 setup(
