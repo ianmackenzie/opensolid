41b5894d77af787c11c463677419ce98cf3e3492
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-10-03 11:01:08 -0400

    Let Fourmolu tweak formatting

--- opensolid-core/src/OpenSolid/DivisionByZero.hs
+++ opensolid-core/src/OpenSolid/DivisionByZero.hs
@@ -1,6 +1,6 @@
-module OpenSolid.DivisionByZero (DivisionByZero(DivisionByZero)) where
+module OpenSolid.DivisionByZero (DivisionByZero (DivisionByZero)) where
 
-import OpenSolid.Prelude
 import OpenSolid.Error qualified as Error
+import OpenSolid.Prelude
 
 data DivisionByZero = DivisionByZero deriving (Eq, Show, Error.Message)
