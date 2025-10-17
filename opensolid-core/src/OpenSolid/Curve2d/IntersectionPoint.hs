b35e5ae46582813b73cdfe3c401ed644599a0065
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-10-17 10:03:22 -0400

    Tweak naming

--- opensolid-core/src/OpenSolid/Curve2d/IntersectionPoint.hs
+++ opensolid-core/src/OpenSolid/Curve2d/IntersectionPoint.hs
@@ -1,8 +1,8 @@
 module OpenSolid.Curve2d.IntersectionPoint
-  ( IntersectionPoint (Crossing, Tangent, Continuation)
+  ( IntersectionPoint (Crossing, Tangent, Corner)
   , crossing
   , tangent
-  , continuation
+  , corner
   , parameterValues
   )
 where
@@ -12,7 +12,7 @@ import OpenSolid.Prelude
 data IntersectionPoint
   = Crossing Float Float Sign
   | Tangent Float Float Sign
-  | Continuation Float Float
+  | Corner Float Float
   deriving (Eq, Ord, Show)
 
 instance ApproximateEquality IntersectionPoint IntersectionPoint Unitless where
@@ -20,8 +20,8 @@ instance ApproximateEquality IntersectionPoint IntersectionPoint Unitless where
   Crossing{} ~= _ = False
   Tangent u1 v1 sign1 ~= Tangent u2 v2 sign2 = u1 ~= u2 && v1 ~= v2 && sign1 == sign2
   Tangent{} ~= _ = False
-  Continuation u1 v1 ~= Continuation u2 v2 = u1 ~= u2 && v1 ~= v2
-  Continuation{} ~= _ = False
+  Corner u1 v1 ~= Corner u2 v2 = u1 ~= u2 && v1 ~= v2
+  Corner{} ~= _ = False
 
 crossing :: Float -> Float -> Sign -> IntersectionPoint
 crossing = Crossing
@@ -29,10 +29,10 @@ crossing = Crossing
 tangent :: Float -> Float -> Sign -> IntersectionPoint
 tangent = Tangent
 
-continuation :: Float -> Float -> IntersectionPoint
-continuation = Continuation
+corner :: Float -> Float -> IntersectionPoint
+corner = Corner
 
 parameterValues :: IntersectionPoint -> (Float, Float)
 parameterValues (Crossing t1 t2 _) = (t1, t2)
 parameterValues (Tangent t1 t2 _) = (t1, t2)
-parameterValues (Continuation t1 t2) = (t1, t2)
+parameterValues (Corner t1 t2) = (t1, t2)
