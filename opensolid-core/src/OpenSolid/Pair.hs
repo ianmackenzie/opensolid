4060a69dd8dbec63d8f47220c68d6b20027ef45e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-11-11 19:50:19 -0500

    Add Pair.map helper function

--- opensolid-core/src/OpenSolid/Pair.hs
+++ opensolid-core/src/OpenSolid/Pair.hs
@@ -1,6 +1,7 @@
 module OpenSolid.Pair
   ( first
   , second
+  , map
   , mapFirst
   , mapSecond
   , decorate
@@ -14,6 +15,10 @@ first (a, _) = a
 {-# INLINE second #-}
 second :: (a, b) -> b
 second (_, b) = b
+
+{-# INLINE map #-}
+map :: (a -> b) -> (a, a) -> (b, b)
+map f (a1, a2) = (f a1, f a2)
 
 mapFirst :: (a1 -> a2) -> (a1, b) -> (a2, b)
 mapFirst f (a, b) = (f a, b)
