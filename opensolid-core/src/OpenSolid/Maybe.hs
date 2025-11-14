c718698036bdf01e1f402811c654e90a8e769579
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-11-13 19:58:25 -0500

    'Flatten' code slightly

--- opensolid-core/src/OpenSolid/Maybe.hs
+++ opensolid-core/src/OpenSolid/Maybe.hs
@@ -14,8 +14,9 @@ map :: (a -> b) -> Maybe a -> Maybe b
 map = Prelude.fmap
 
 map2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
-map2 function (Just first) second = map (function first) second
+map2 function (Just first) (Just second) = Just (function first second)
 map2 _ Nothing _ = Nothing
+map2 _ _ Nothing = Nothing
 
 oneOf :: List (Maybe a) -> Maybe a
 oneOf maybes = case maybes of
