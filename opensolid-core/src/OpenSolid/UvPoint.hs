2d7d6caabc536cc32a4e8fe8e3d295902f030eb5
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-12-04 22:32:03 -0500

    Fill out UvPoint module
    Try to avoid the need for using Point2d module at all
    when working with UvPoint values

--- opensolid-core/src/OpenSolid/UvPoint.hs
+++ opensolid-core/src/OpenSolid/UvPoint.hs
@@ -1,22 +1,67 @@
-module OpenSolid.UvPoint (UvPoint, samples, random) where
+module OpenSolid.UvPoint
+  ( UvPoint
+  , pattern UvPoint
+  , origin
+  , coordinates
+  , uCoordinate
+  , vCoordinate
+  , distanceFrom
+  , midpoint
+  , samples
+  , random
+  )
+where
 
 import OpenSolid.Parameter qualified as Parameter
 import OpenSolid.Point2d (Point2d (Point2d))
+import OpenSolid.Point2d qualified as Point2d
 import OpenSolid.Prelude
 import OpenSolid.Quadrature qualified as Quadrature
 import OpenSolid.Random qualified as Random
 
+-- | A point in UV parameter space.
 type UvPoint = Point2d Unitless UvSpace
+
+{-# COMPLETE UvPoint #-}
+
+-- | Construct a point from its U and V coordinates.
+{-# INLINE UvPoint #-}
+pattern UvPoint :: Number -> Number -> UvPoint
+pattern UvPoint u v = Point2d u v
+
+-- | The UV point with coordinates (0, 0).
+origin :: UvPoint
+origin = Point2d.origin
+
+-- | Get the U and V coordinates of a point as a tuple.
+coordinates :: UvPoint -> (Number, Number)
+coordinates = Point2d.coordinates
+
+-- | Get the U coordinate of a point.
+uCoordinate :: UvPoint -> Number
+uCoordinate = Point2d.xCoordinate
+
+-- | Get the V coordinate of a point.
+vCoordinate :: UvPoint -> Number
+vCoordinate = Point2d.yCoordinate
+
+-- | Compute the distance in UV space from one point to another.
+distanceFrom :: UvPoint -> UvPoint -> Number
+distanceFrom = Point2d.distanceFrom
+
+-- | Find the midpoint between two points.
+midpoint :: UvPoint -> UvPoint -> UvPoint
+midpoint = Point2d.midpoint
 
 samples :: List UvPoint
 samples = do
   let (t1, t2, t3, t4, t5) = Quadrature.abscissae5
-  let p1 = Point2d t3 t3
-  let p2 = Point2d t2 t1
-  let p3 = Point2d t5 t2
-  let p4 = Point2d t4 t5
-  let p5 = Point2d t1 t4
+  let p1 = UvPoint t3 t3
+  let p2 = UvPoint t2 t1
+  let p3 = UvPoint t5 t2
+  let p4 = UvPoint t4 t5
+  let p5 = UvPoint t1 t4
   [p1, p2, p3, p4, p5]
 
 random :: Random.Generator UvPoint
-random = Random.map2 Point2d Parameter.random Parameter.random
+random = Random.map2 UvPoint Parameter.random Parameter.random
