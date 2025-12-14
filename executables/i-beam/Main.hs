79827c2985bfb5c3ced588ca12cda4324e9e939c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2025-12-13 22:28:39 -0500

    Update Haskell I-beam example to use Region2d.fillet

--- executables/i-beam/Main.hs
+++ executables/i-beam/Main.hs
@@ -3,15 +3,14 @@ module Main (main) where
 import OpenSolid.Axis2d qualified as Axis2d
 import OpenSolid.Body3d qualified as Body3d
 import OpenSolid.Color qualified as Color
-import OpenSolid.Curve2d qualified as Curve2d
-import OpenSolid.Direction2d qualified as Direction2d
 import OpenSolid.Gltf qualified as Gltf
 import OpenSolid.Length qualified as Length
-import OpenSolid.List qualified as List
 import OpenSolid.Model3d qualified as Model3d
+import OpenSolid.NonEmpty qualified as NonEmpty
 import OpenSolid.PbrMaterial qualified as PbrMaterial
 import OpenSolid.Point2D (pattern Point2D)
 import OpenSolid.Point2D qualified as Point2D
+import OpenSolid.Polygon2d (Polygon2d (Polygon2d))
 import OpenSolid.Prelude
 import OpenSolid.Region2d qualified as Region2d
 import OpenSolid.Resolution qualified as Resolution
@@ -27,27 +26,22 @@ main = Tolerance.using Length.nanometer do
   let height = Length.centimeters 15
   let thickness = Length.centimeters 2
   let filletRadius = Length.millimeters 5
-  let p1 = Point2D.x (0.5 *. thickness)
-  let p2 = Point2D (0.5 *. thickness) (0.5 *. height .-. thickness)
-  let p3 = Point2D (0.5 *. width) (0.5 *. height .-. thickness)
-  let p4 = Point2D (0.5 *. width) (0.5 *. height)
-  let p5 = Point2D.y (0.5 *. height)
-  let fillet =
-        Curve2d.cornerArc
-          p2
-          (#incoming Direction2d.y)
-          (#outgoing Direction2d.x)
-          (#radius filletRadius)
-  let topRightCurves =
-        [ Curve2d.lineFrom p1 fillet.startPoint
-        , fillet
-        , Curve2d.lineFrom fillet.endPoint p3
-        , Curve2d.lineFrom p3 p4
-        , Curve2d.lineFrom p4 p5
+  let webTopRight = Point2D (0.5 *. thickness) (0.5 *. height .-. thickness)
+  let topFlangeBottomRight = Point2D (0.5 *. width) (0.5 *. height .-. thickness)
+  let topFlangeTopRight = Point2D (0.5 *. width) (0.5 *. height)
+  let topRightPoints = NonEmpty.three webTopRight topFlangeBottomRight topFlangeTopRight
+  let topLeftPoints = NonEmpty.reverseMap (Point2D.mirrorAcross Axis2d.y) topRightPoints
+  let topPoints = topRightPoints <> topLeftPoints
+  let bottomPoints = NonEmpty.reverseMap (Point2D.mirrorAcross Axis2d.x) topPoints
+  let allPoints = topPoints <> bottomPoints
+  let filletPoints =
+        [ webTopRight
+        , webTopRight & Point2D.mirrorAcross Axis2d.y
+        , webTopRight & Point2D.mirrorAcross Axis2d.x
+        , webTopRight & Point2D.mirrorAcross Axis2d.y & Point2D.mirrorAcross Axis2d.x
         ]
-  let topCurves = topRightCurves <> List.map (Curve2d.mirrorAcross Axis2d.y) topRightCurves
-  let allCurves = topCurves <> List.map (Curve2d.mirrorAcross Axis2d.x) topCurves
-  profile <- Result.orFail (Region2d.boundedBy allCurves)
+  baseProfile <- Result.orFail (Region2d.polygon (Polygon2d allPoints))
+  profile <- Result.orFail (Region2d.fillet filletPoints (#radius filletRadius) baseProfile)
   body <- Result.orFail (Body3d.extruded World3d.frontPlane profile (-0.5 *. length) (0.5 *. length))
   let material = PbrMaterial.metal (Color.rgb1 0.913 0.921 0.925) (#roughness 0.3)
   let model = Model3d.bodyWith [Model3d.pbrMaterial material] body
