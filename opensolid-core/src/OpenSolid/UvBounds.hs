e5ab49349f69a6c3ec7f90e20a3858cc1906e4e9
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-01-24 19:43:35 -0500

    Add UvBounds pattern for construction/destructuring

--- opensolid-core/src/OpenSolid/UvBounds.hs
+++ opensolid-core/src/OpenSolid/UvBounds.hs
@@ -1,10 +1,23 @@
-module OpenSolid.UvBounds (UvBounds, unitSquare) where
+module OpenSolid.UvBounds
+  ( UvBounds
+  , pattern UvBounds
+  , unitSquare
+  )
+where
 
 import OpenSolid.Bounds2D (Bounds2D (Bounds2D))
+import OpenSolid.Interval (Interval)
 import OpenSolid.Interval qualified as Interval
 import OpenSolid.Prelude
 
 type UvBounds = Bounds2D Unitless UvSpace
+
+{-# COMPLETE UvBounds #-}
+
+-- | Construct a UV bounding box from its U and V coordinate intervals.
+{-# INLINE UvBounds #-}
+pattern UvBounds :: Interval Unitless -> Interval Unitless -> UvBounds
+pattern UvBounds u v = Bounds2D u v
 
 unitSquare :: UvBounds
 unitSquare = Bounds2D Interval.unit Interval.unit
