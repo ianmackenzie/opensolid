a0cfdf91c46c36cd9005384ce6dbb5f79064ae33
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-02-06 11:24:18 -0500

    Start filling out generic VectorBounds module

--- opensolid-core/src/OpenSolid/VectorBounds.hs
+++ opensolid-core/src/OpenSolid/VectorBounds.hs
@@ -1,12 +1,22 @@
 module OpenSolid.VectorBounds
   ( VectorBounds
+  , Exists
+  , includes
+  , center
   )
 where
 
 import Data.Void (Void)
 import OpenSolid.Interval (Interval)
+import OpenSolid.Prelude
+import OpenSolid.Units (HasUnits)
+import OpenSolid.Units qualified as Units
+import OpenSolid.Vector (Vector)
+import OpenSolid.Vector qualified as Vector
 import OpenSolid.VectorBounds2D (VectorBounds2D)
+import OpenSolid.VectorBounds2D qualified as VectorBounds2D
 import OpenSolid.VectorBounds3D (VectorBounds3D)
+import OpenSolid.VectorBounds3D qualified as VectorBounds3D
 
 type family
   VectorBounds dimension units space =
@@ -15,3 +25,47 @@ type family
   VectorBounds 1 units Void = Interval units
   VectorBounds 2 units space = VectorBounds2D units space
   VectorBounds 3 units space = VectorBounds3D units space
+
+class
+  ( Vector.Exists dimension units space
+  , Exists dimension Unitless space
+  , HasUnits (VectorBounds dimension units space) units
+  , Units.Coercion (VectorBounds dimension units space) (VectorBounds dimension Unitless space)
+  , Units.Coercion (VectorBounds dimension Unitless space) (VectorBounds dimension units space)
+  , DotMultiplication
+      (VectorBounds dimension units space)
+      (Vector dimension Unitless space)
+      (Interval units)
+  , DotMultiplication
+      (Vector dimension Unitless space)
+      (VectorBounds dimension units space)
+      (Interval units)
+  , DotMultiplication
+      (VectorBounds dimension Unitless space)
+      (Vector dimension units space)
+      (Interval units)
+  , DotMultiplication
+      (Vector dimension units space)
+      (VectorBounds dimension Unitless space)
+      (Interval units)
+  , DotMultiplication
+      (VectorBounds dimension units space)
+      (VectorBounds dimension Unitless space)
+      (Interval units)
+  , DotMultiplication
+      (VectorBounds dimension Unitless space)
+      (VectorBounds dimension units space)
+      (Interval units)
+  ) =>
+  Exists dimension units space
+  where
+  includes :: Vector dimension units space -> VectorBounds dimension units space -> Bool
+  center :: VectorBounds dimension units space -> Vector dimension units space
+
+instance Exists 2 units space where
+  includes = VectorBounds2D.includes
+  center = VectorBounds2D.center
+
+instance Exists 3 units space where
+  includes = VectorBounds3D.includes
+  center = VectorBounds3D.center
