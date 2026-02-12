33cb328e7fc3200359491d8a44753d39980cba11
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-02-11 20:24:10 -0500

    Switch one test executable over to using new OpenSolid module qualified
    Make sure it's reasonably straightforward to write useful programs using OpenSolid
    without needing to enable RebindableSyntax and import OpenSolid.Prelude

--- executables/body3d-meshing/Main.hs
+++ executables/body3d-meshing/Main.hs
@@ -1,18 +1,22 @@
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# LANGUAGE ImplicitPrelude #-}
+
 module Main (main) where
 
+import OpenSolid qualified
 import OpenSolid.Angle qualified as Angle
 import OpenSolid.Body3D qualified as Body3D
 import OpenSolid.Convention3D qualified as Convention3D
 import OpenSolid.Curve2D qualified as Curve2D
 import OpenSolid.Length qualified as Length
 import OpenSolid.Point2D qualified as Point2D
-import OpenSolid.Prelude
 import OpenSolid.Region2D qualified as Region2D
 import OpenSolid.Resolution qualified as Resolution
 import OpenSolid.Result qualified as Result
 import OpenSolid.Stl qualified as Stl
 import OpenSolid.Tolerance qualified as Tolerance
 import OpenSolid.World3D qualified as World3D
+import Prelude hiding (length)
 
 main :: IO ()
 main = Tolerance.using Length.nanometer do
@@ -26,7 +30,9 @@ main = Tolerance.using Length.nanometer do
           (#endAngle (Angle.degrees 225.0))
   let line = Curve2D.lineFrom arc.endPoint arc.startPoint
   profile <- Result.orFail (Region2D.boundedBy [arc, line])
-  body <- Result.orFail (Body3D.extruded World3D.frontPlane profile (-0.5 * length) (0.5 * length))
+  let extrusionStart = OpenSolid.product (OpenSolid.number -0.5) length
+  let extrusionEnd = OpenSolid.product (OpenSolid.number 0.5) length
+  body <- Result.orFail (Body3D.extruded World3D.frontPlane profile extrusionStart extrusionEnd)
   let resolution = Resolution.maxSize (Length.centimeters 30.0)
   let mesh = Body3D.toPointMesh resolution body
   let outputPath = "executables/body3D-meshing/mesh.stl"
