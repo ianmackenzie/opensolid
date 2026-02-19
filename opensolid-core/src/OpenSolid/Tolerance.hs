3b9e9aedb7321dfb76029f55ac591d4f5aa6d3dc
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-02-18 20:51:02 -0500

    Add a couple reasonable 'default' tolerance values to Tolerance module

--- opensolid-core/src/OpenSolid/Tolerance.hs
+++ opensolid-core/src/OpenSolid/Tolerance.hs
@@ -1,10 +1,20 @@
 module OpenSolid.Tolerance
   ( Tolerance
   , using
+  , unitless
+  , angular
   )
 where
 
+import OpenSolid.Angle (Angle)
+import OpenSolid.Angle qualified as Angle
 import OpenSolid.Prelude
 
 using :: Quantity units -> (Tolerance units => a) -> a
 using tolerance expression = let ?tolerance = tolerance in expression
+
+unitless :: Number
+unitless = 1e-9
+
+angular :: Angle
+angular = Angle.radians unitless
