2562e5dfc049b0234f570def6c2b8923fd92c225
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-02-27 23:05:10 -0500

    Add NewtonRaphson3D.surface
    Will allow solving of e.g. 3D curve intersections
    (since that can be though of as solving for zeros of
    a 3D displacement function of a 2D 'surface' parameter)

--- opensolid-core/src/OpenSolid/NewtonRaphson3D.hs
+++ opensolid-core/src/OpenSolid/NewtonRaphson3D.hs
@@ -1,6 +1,8 @@
-module OpenSolid.NewtonRaphson3D (curve) where
+module OpenSolid.NewtonRaphson3D (curve, surface) where
 
 import OpenSolid.Prelude
+import OpenSolid.UvPoint (UvPoint)
+import OpenSolid.Vector2D (Vector2D (Vector2D))
 import OpenSolid.Vector3D (Vector3D)
 import OpenSolid.Vector3D qualified as Vector3D
 
@@ -20,3 +22,36 @@ curveImpl function derivative t1 v1 = do
   if Vector3D.squaredMagnitude_ v2 < 0.25 * Vector3D.squaredMagnitude_ v1
     then curveImpl function derivative t2 v2
     else t1
+
+surface ::
+  (UvPoint -> Vector3D units space) ->
+  (UvPoint -> Vector3D units space) ->
+  (UvPoint -> Vector3D units space) ->
+  UvPoint ->
+  UvPoint
+surface function uDerivative vDerivative uvPoint =
+  surfaceImpl function uDerivative vDerivative uvPoint (function uvPoint)
+
+surfaceImpl ::
+  (UvPoint -> Vector3D units space) ->
+  (UvPoint -> Vector3D units space) ->
+  (UvPoint -> Vector3D units space) ->
+  UvPoint ->
+  Vector3D units space ->
+  UvPoint
+surfaceImpl function uDerivative vDerivative uvPoint1 value1 = do
+  let du = uDerivative uvPoint1
+  let dv = vDerivative uvPoint1
+  let uu = du `dot_` du
+  let uv = du `dot_` dv
+  let vv = dv `dot_` dv
+  let determinant = uu ?*? vv - uv ?*? uv
+  let x = value1 `dot_` du
+  let y = value1 `dot_` dv
+  let uStep = (uv ?*? y - vv ?*? x) / determinant
+  let vStep = (uv ?*? x - uu ?*? y) / determinant
+  let uvPoint2 = uvPoint1 + (Vector2D uStep vStep)
+  let value2 = function uvPoint2
+  if Vector3D.squaredMagnitude_ value2 < 0.25 * Vector3D.squaredMagnitude_ value1
+    then surfaceImpl function uDerivative vDerivative uvPoint2 value2
+    else uvPoint1
