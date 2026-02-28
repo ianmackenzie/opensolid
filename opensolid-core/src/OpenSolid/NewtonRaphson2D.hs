586a4f6b6b6bcb5200c68a7d3020541af7c56b3e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-02-27 23:05:10 -0500

    Tweak Newton-Raphson termination logic for consistency
    1D solving checks for error magnitude being cut by at least a factor 2,
    so *squared* error should be cut by at least a factor of 4

--- opensolid-core/src/OpenSolid/NewtonRaphson2D.hs
+++ opensolid-core/src/OpenSolid/NewtonRaphson2D.hs
@@ -18,7 +18,7 @@ curveImpl function derivative t1 v1 = do
   let d1 = derivative t1
   let t2 = t1 - (v1 `dot_` d1) / (d1 `dot_` d1)
   let v2 = function t2
-  if Vector2D.squaredMagnitude_ v2 < 0.5 * Vector2D.squaredMagnitude_ v1
+  if Vector2D.squaredMagnitude_ v2 < 0.25 * Vector2D.squaredMagnitude_ v1
     then curveImpl function derivative t2 v2
     else t1
 
@@ -47,6 +47,6 @@ surfaceImpl function uDerivative vDerivative uvPoint1 value1 = do
   let vStep = (dydu1 ?*? x1 - dxdu1 ?*? y1) / determinant
   let uvPoint2 = uvPoint1 + Vector2D uStep vStep
   let value2 = function uvPoint2
-  if Vector2D.squaredMagnitude_ value2 < 0.5 * Vector2D.squaredMagnitude_ value1
+  if Vector2D.squaredMagnitude_ value2 < 0.25 * Vector2D.squaredMagnitude_ value1
     then surfaceImpl function uDerivative vDerivative uvPoint2 value2
     else uvPoint1
