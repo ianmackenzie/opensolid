be9475b3fbd9df13ab6614281adda966ef371901
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-02-27 23:05:10 -0500

    Add generic NewtonRaphson.Surface typeclass
    Allow solving 'surface' problems (problems of two parameters)
    in a dimension-independent way

--- opensolid-core/src/OpenSolid/NewtonRaphson.hs
+++ opensolid-core/src/OpenSolid/NewtonRaphson.hs
@@ -3,6 +3,8 @@
 module OpenSolid.NewtonRaphson
   ( Curve
   , curve
+  , Surface
+  , surface
   )
 where
 
@@ -11,6 +13,7 @@ import OpenSolid.NewtonRaphson1D qualified as NewtonRaphson1D
 import OpenSolid.NewtonRaphson2D qualified as NewtonRaphson2D
 import OpenSolid.NewtonRaphson3D qualified as NewtonRaphson3D
 import OpenSolid.Prelude
+import OpenSolid.UvPoint (UvPoint)
 import OpenSolid.Vector (Vector)
 
 class Curve dimension units space where
@@ -27,3 +30,15 @@ instance Curve 2 units space where
 
 instance Curve 3 units space where
   curve = NewtonRaphson3D.curve
+
+class Surface dimension units space where
+  surface ::
+    (UvPoint -> (# Vector dimension units space, Vector dimension units space, Vector dimension units space #)) ->
+    UvPoint ->
+    UvPoint
+
+instance Surface 2 units space where
+  surface = NewtonRaphson2D.surface
+
+instance Surface 3 units space where
+  surface = NewtonRaphson3D.surface
