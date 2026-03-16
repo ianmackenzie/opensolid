b97bc9cca34de9824a3419aeaece511b103a47ba
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-03-15 21:14:13 -0400

    Update Curve.Nonzero module to use new Curve.CurvatureVector module

--- opensolid-core/src/OpenSolid/Curve/Nonzero.hs
+++ opensolid-core/src/OpenSolid/Curve/Nonzero.hs
@@ -7,12 +7,11 @@ where
 
 import OpenSolid.Curve (Curve)
 import OpenSolid.Curve qualified as Curve
+import OpenSolid.Curve.CurvatureVector qualified as Curve.CurvatureVector
 import OpenSolid.Curve1D qualified as Curve1D
 import OpenSolid.Interval (Interval)
-import OpenSolid.Interval qualified as Interval
 import OpenSolid.Nonzero (Nonzero (Nonzero))
 import OpenSolid.Prelude
-import OpenSolid.Quantity qualified as Quantity
 import OpenSolid.Vector (Vector)
 import OpenSolid.Vector qualified as Vector
 import OpenSolid.VectorBounds (VectorBounds)
@@ -38,25 +37,17 @@ curvatureVectorValue_ ::
   Nonzero (Curve dimension units space) ->
   Number ->
   Vector dimension (Unitless ?/? units) space
-curvatureVectorValue_ (Nonzero curve) tValue = do
-  let derivativeValue = Curve.derivativeValue curve tValue
-  let secondDerivativeValue = Curve.secondDerivativeValue curve tValue
-  let derivativeSquaredMagnitude_ = Vector.squaredMagnitude_ derivativeValue
-  let numerator =
-        secondDerivativeValue
-          - derivativeValue * ((secondDerivativeValue `dot_` derivativeValue) / derivativeSquaredMagnitude_)
-  Vector.unerase (Vector.erase numerator / Quantity.erase derivativeSquaredMagnitude_)
+curvatureVectorValue_ (Nonzero curve) tValue =
+  Curve.CurvatureVector.value_
+    (Curve.derivativeValue curve tValue)
+    (Curve.secondDerivativeValue curve tValue)
 
 curvatureVectorBounds_ ::
   (Curve.Exists dimension units space, VectorBounds.Exists dimension (Unitless ?/? units) space) =>
   Nonzero (Curve dimension units space) ->
   Interval Unitless ->
   VectorBounds dimension (Unitless ?/? units) space
-curvatureVectorBounds_ (Nonzero curve) tValue = do
-  let derivativeBounds = Curve.derivativeBounds curve tValue
-  let secondDerivativeBounds = Curve.secondDerivativeBounds curve tValue
-  let derivativeSquaredMagnitude_ = VectorBounds.squaredMagnitude_ derivativeBounds
-  let numerator =
-        secondDerivativeBounds
-          - derivativeBounds * ((secondDerivativeBounds `dot_` derivativeBounds) / derivativeSquaredMagnitude_)
-  VectorBounds.unerase (VectorBounds.erase numerator / Interval.erase derivativeSquaredMagnitude_)
+curvatureVectorBounds_ (Nonzero curve) tBounds =
+  Curve.CurvatureVector.bounds_
+    (Curve.derivativeBounds curve tBounds)
+    (Curve.secondDerivativeBounds curve tBounds)
