c69b08fb00f23aa69cd419bb180846630445e972
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-03-27 21:42:43 -0400

    Add initial Arc3D and Circle3D modules

---
+++ opensolid-core/src/OpenSolid/Arc3D.hs
@@ -0,0 +1,105 @@
+module OpenSolid.Arc3D
+  ( Arc3D
+  , on
+  , plane
+  , centerPoint
+  , radius
+  , startAngle
+  , endAngle
+  , sweptAngle
+  , startPoint
+  , endPoint
+  , toCircle
+  , reverse
+  , transformBy
+  , translateBy
+  , translateIn
+  , translateAlong
+  , rotateAround
+  , mirrorAcross
+  )
+where
+
+import OpenSolid.Angle (Angle)
+import OpenSolid.Arc2D (Arc2D)
+import OpenSolid.Arc2D qualified as Arc2D
+import OpenSolid.Axis3D (Axis3D)
+import OpenSolid.Circle2D qualified as Circle2D
+import OpenSolid.Circle3D (Circle3D)
+import OpenSolid.Circle3D qualified as Circle3D
+import OpenSolid.Direction3D (Direction3D)
+import OpenSolid.Length (Length)
+import OpenSolid.Plane3D (Plane3D)
+import OpenSolid.Plane3D qualified as Plane3D
+import OpenSolid.Point2D qualified as Point2D
+import OpenSolid.Point3D (Point3D)
+import OpenSolid.Point3D qualified as Point3D
+import OpenSolid.Prelude
+import OpenSolid.Quantity qualified as Quantity
+import OpenSolid.Transform qualified as Transform
+import OpenSolid.Transform3D (Transform3D)
+import OpenSolid.Transform3D qualified as Transform3D
+import OpenSolid.Vector3D (Vector3D)
+
+data Arc3D space where
+  Arc3D :: Plane3D space local -> Length -> Angle -> Angle -> Arc3D space
+
+on :: Plane3D space local -> Arc2D Meters local -> Arc3D space
+on givenPlane givenArc = do
+  let originPoint = Point3D.on givenPlane (Arc2D.centerPoint givenArc)
+  Arc3D
+    (Plane3D.moveTo originPoint givenPlane)
+    (Arc2D.radius givenArc)
+    (Arc2D.startAngle givenArc)
+    (Arc2D.endAngle givenArc)
+
+plane :: Arc3D space -> Plane3D space local
+plane (Arc3D p _ _ _) = Plane3D.coerce p
+
+centerPoint :: Arc3D space -> Point3D space
+centerPoint arc = Plane3D.originPoint (plane arc)
+
+radius :: Arc3D space -> Length
+radius (Arc3D _ r _ _) = r
+
+startAngle :: Arc3D space -> Angle
+startAngle (Arc3D _ _ a _) = a
+
+endAngle :: Arc3D space -> Angle
+endAngle (Arc3D _ _ _ b) = b
+
+sweptAngle :: Arc3D space -> Angle
+sweptAngle arc = endAngle arc - startAngle arc
+
+point :: Arc3D space -> Number -> Point3D space
+point (Arc3D p r a b) t = Point3D.on p (Point2D.polar r (Quantity.interpolateFrom a b t))
+
+startPoint :: Arc3D space -> Point3D space
+startPoint arc = point arc 0.0
+
+endPoint :: Arc3D space -> Point3D space
+endPoint arc = point arc 1.0
+
+toCircle :: Arc3D space -> Circle3D space
+toCircle arc = Circle3D.on (plane arc) (Circle2D.withRadius (radius arc) Point2D.origin)
+
+reverse :: Arc3D space -> Arc3D space
+reverse (Arc3D p r a b) = Arc3D p r b a
+
+transformBy :: Transform.IsOrthonormal tag => Transform3D tag space -> Arc3D space -> Arc3D space
+transformBy transform (Arc3D p r a b) = Arc3D (Plane3D.transformBy transform p) r a b
+
+translateBy :: Vector3D Meters space -> Arc3D space -> Arc3D space
+translateBy = Transform3D.translateByImpl transformBy
+
+translateIn :: Direction3D space -> Length -> Arc3D space -> Arc3D space
+translateIn = Transform3D.translateInImpl transformBy
+
+translateAlong :: Axis3D space -> Length -> Arc3D space -> Arc3D space
+translateAlong = Transform3D.translateAlongImpl transformBy
+
+rotateAround :: Axis3D space -> Angle -> Arc3D space -> Arc3D space
+rotateAround = Transform3D.rotateAroundImpl transformBy
+
+mirrorAcross :: Plane3D space local -> Arc3D space -> Arc3D space
+mirrorAcross = Transform3D.mirrorAcrossImpl transformBy
---
+++ opensolid-core/src/OpenSolid/Circle3D.hs
@@ -0,0 +1,43 @@
+module OpenSolid.Circle3D
+  ( Circle3D
+  , on
+  , centerPoint
+  , radius
+  , diameter
+  , normalDirection
+  , plane
+  )
+where
+
+import OpenSolid.Circle2D (Circle2D)
+import OpenSolid.Circle2D qualified as Circle2D
+import OpenSolid.Direction3D (Direction3D)
+import OpenSolid.Length (Length)
+import OpenSolid.Plane3D (Plane3D)
+import OpenSolid.Plane3D qualified as Plane3D
+import OpenSolid.Point3D (Point3D)
+import OpenSolid.Point3D qualified as Point3D
+import OpenSolid.Prelude
+
+data Circle3D space where
+  Circle3D :: Plane3D space local -> Length -> Circle3D space
+
+on :: Plane3D space local -> Circle2D Meters local -> Circle3D space
+on givenPlane givenCircle = do
+  let originPoint = Point3D.on givenPlane (Circle2D.centerPoint givenCircle)
+  Circle3D (Plane3D.moveTo originPoint givenPlane) (Circle2D.radius givenCircle)
+
+centerPoint :: Circle3D space -> Point3D space
+centerPoint circle = Plane3D.originPoint (plane circle)
+
+radius :: Circle3D space -> Length
+radius (Circle3D _ r) = r
+
+diameter :: Circle3D space -> Length
+diameter circle = 2.0 * radius circle
+
+normalDirection :: Circle3D space -> Direction3D space
+normalDirection circle = Plane3D.normalDirection (plane circle)
+
+plane :: Circle3D space -> Plane3D space local
+plane (Circle3D p _) = Plane3D.coerce p
