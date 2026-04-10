ab90d60ce0a64fc97554194ac2cb950103c876e1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-04-09 22:23:07 -0400

    Remove Surface3D meshing code
    Not really used anywhere, and implementation is a bit old/creaky
    (the Body3D meshing algorithms are a bit more up to date)

--- opensolid-core/src/OpenSolid/Surface3D.hs
+++ opensolid-core/src/OpenSolid/Surface3D.hs
@@ -16,66 +16,39 @@ module OpenSolid.Surface3D
   , flip
   , placeIn
   , relativeTo
-  , toMesh
   )
 where
 
 import OpenSolid.Angle (Angle)
 import OpenSolid.Axis2D (Axis2D)
 import OpenSolid.Axis2D qualified as Axis2D
-import OpenSolid.Bounds2D (Bounds2D (Bounds2D))
 import OpenSolid.Bounds2D qualified as Bounds2D
 import OpenSolid.Bounds3D (Bounds3D)
-import OpenSolid.CDT qualified as CDT
 import OpenSolid.Curve1D qualified as Curve1D
 import OpenSolid.Curve2D (Curve2D)
 import OpenSolid.Curve2D qualified as Curve2D
 import OpenSolid.Curve3D (Curve3D)
-import OpenSolid.Curve3D qualified as Curve3D
-import OpenSolid.Domain1D qualified as Domain1D
 import OpenSolid.Frame2D qualified as Frame2D
 import OpenSolid.Frame3D (Frame3D)
 import OpenSolid.Frame3D qualified as Frame3D
-import OpenSolid.Interval (Interval)
-import OpenSolid.Interval qualified as Interval
-import OpenSolid.Length (Length)
-import OpenSolid.Line2D (Line2D)
-import OpenSolid.Line2D qualified as Line2D
-import OpenSolid.Linearization qualified as Linearization
 import OpenSolid.List qualified as List
-import OpenSolid.Mesh (Mesh)
-import OpenSolid.Mesh qualified as Mesh
 import OpenSolid.NonEmpty qualified as NonEmpty
 import OpenSolid.Plane3D (Plane3D)
 import OpenSolid.Plane3D qualified as Plane3D
 import OpenSolid.Point2D qualified as Point2D
-import OpenSolid.Point3D (Point3D)
-import OpenSolid.Polygon2D (Polygon2D (Polygon2D))
-import OpenSolid.Polygon2D qualified as Polygon2D
 import OpenSolid.Prelude
-import OpenSolid.Quantity qualified as Quantity
 import OpenSolid.Region2D (Region2D)
 import OpenSolid.Region2D qualified as Region2D
-import OpenSolid.Set2D (Set2D)
-import OpenSolid.Set2D qualified as Set2D
 import OpenSolid.SurfaceCurve3D (SurfaceCurve3D)
 import OpenSolid.SurfaceCurve3D qualified as SurfaceCurve3D
 import OpenSolid.SurfaceFunction1D qualified as SurfaceFunction1D
 import OpenSolid.SurfaceFunction2D qualified as SurfaceFunction2D
 import OpenSolid.SurfaceFunction3D (SurfaceFunction3D)
 import OpenSolid.SurfaceFunction3D qualified as SurfaceFunction3D
-import OpenSolid.SurfaceLinearization qualified as SurfaceLinearization
-import OpenSolid.SurfaceParameter (SurfaceParameter (U, V))
-import OpenSolid.Tolerance qualified as Tolerance
-import OpenSolid.UvBounds (UvBounds)
-import OpenSolid.UvPoint (UvPoint, pattern UvPoint)
 import OpenSolid.UvRegion (UvRegion)
 import OpenSolid.UvRegion qualified as UvRegion
 import OpenSolid.Vector3D (Vector3D)
-import OpenSolid.VectorBounds3D qualified as VectorBounds3D
 import OpenSolid.VectorCurve3D (VectorCurve3D)
-import OpenSolid.VectorSurfaceFunction3D (VectorSurfaceFunction3D)
-import OpenSolid.VectorSurfaceFunction3D qualified as VectorSurfaceFunction3D
 
 data Surface3D space = Surface3D
   { function :: SurfaceFunction3D space
@@ -180,121 +153,3 @@ placeIn frame surface =
 relativeTo :: Frame3D global local -> Surface3D global -> Surface3D local
 relativeTo frame surface =
   parametric (SurfaceFunction3D.relativeTo frame surface.function) surface.domain
-
-toMesh :: Length -> Surface3D space -> Mesh (Point3D space)
-toMesh accuracy surface = do
-  let f = surface.function
-  let fuu = f.du & VectorSurfaceFunction3D.derivative U
-  let fuv = f.du & VectorSurfaceFunction3D.derivative V
-  let fvv = f.dv & VectorSurfaceFunction3D.derivative V
-  let boundaryLoops = Region2D.boundaryLoops surface.domain
-  let boundaryPolygons = NonEmpty.map (toPolygon accuracy f fuu fuv fvv) boundaryLoops
-  let boundaryEdges = NonEmpty.combine Polygon2D.edges boundaryPolygons
-  let edgeSet = Set2D.partitionBy Line2D.bounds boundaryEdges
-  let domainBounds = Region2D.bounds surface.domain
-  let steinerPoints = generateSteinerPoints accuracy domainBounds edgeSet fuu fuv fvv []
-  let boundaryVertexLoops = NonEmpty.map Polygon2D.vertices boundaryPolygons
-  let uvMesh = CDT.unsafe boundaryVertexLoops steinerPoints
-  Mesh.map (SurfaceFunction3D.point f) uvMesh
-
-toPolygon ::
-  Length ->
-  SurfaceFunction3D space ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  NonEmpty (Curve2D Unitless) ->
-  Polygon2D Unitless
-toPolygon accuracy f fuu fuv fvv loop =
-  Polygon2D (NonEmpty.combine (boundaryPoints accuracy f fuu fuv fvv) loop)
-
-boundaryPoints ::
-  Length ->
-  SurfaceFunction3D space ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  Curve2D Unitless ->
-  NonEmpty UvPoint
-boundaryPoints accuracy surfaceFunction fuu fuv fvv uvCurve = do
-  let curve3D = surfaceFunction . uvCurve
-  let predicate = linearizationPredicate accuracy fuu fuv fvv uvCurve curve3D
-  let parameterValues = Domain1D.leadingSamplingPoints predicate
-  NonEmpty.map (Curve2D.point uvCurve) parameterValues
-
-surfaceError ::
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  UvBounds ->
-  Length
-surfaceError fuu fuv fvv uvBounds = do
-  let fuuBounds = VectorSurfaceFunction3D.bounds fuu uvBounds
-  let fuvBounds = VectorSurfaceFunction3D.bounds fuv uvBounds
-  let fvvBounds = VectorSurfaceFunction3D.bounds fvv uvBounds
-  SurfaceLinearization.error fuuBounds fuvBounds fvvBounds uvBounds
-
-linearizationPredicate ::
-  Length ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  Curve2D Unitless ->
-  Curve3D space ->
-  Interval Unitless ->
-  Bool
-linearizationPredicate accuracy fuu fuv fvv curve2D curve3D subdomain = do
-  let curveSecondDerivativeBounds = Curve3D.secondDerivativeBounds curve3D subdomain
-  let curveSecondDerivativeMagnitude = VectorBounds3D.magnitude curveSecondDerivativeBounds
-  let uvBounds = Curve2D.bounds curve2D subdomain
-  Linearization.error curveSecondDerivativeMagnitude subdomain <= accuracy
-    && surfaceError fuu fuv fvv uvBounds <= accuracy
-
-generateSteinerPoints ::
-  Length ->
-  UvBounds ->
-  Set2D Unitless (Line2D Unitless) ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  VectorSurfaceFunction3D Meters space ->
-  List UvPoint ->
-  List UvPoint
-generateSteinerPoints accuracy uvBounds edgeSet fuu fuv fvv accumulated = do
-  let Bounds2D uBounds vBounds = uvBounds
-  let recurse = do
-        let (u1, u2) = Interval.bisect uBounds
-        let (v1, v2) = Interval.bisect vBounds
-        let bounds11 = Bounds2D u1 v1
-        let bounds12 = Bounds2D u1 v2
-        let bounds21 = Bounds2D u2 v1
-        let bounds22 = Bounds2D u2 v2
-        accumulated
-          & generateSteinerPoints accuracy bounds11 edgeSet fuu fuv fvv
-          & generateSteinerPoints accuracy bounds12 edgeSet fuu fuv fvv
-          & generateSteinerPoints accuracy bounds21 edgeSet fuu fuv fvv
-          & generateSteinerPoints accuracy bounds22 edgeSet fuu fuv fvv
-  case includeSubdomain uvBounds edgeSet of
-    Unresolved -> recurse
-    Resolved False -> accumulated
-    Resolved True ->
-      if surfaceError fuu fuv fvv uvBounds <= accuracy
-        then UvPoint (Interval.midpoint uBounds) (Interval.midpoint vBounds) : accumulated
-        else recurse
-
-includeSubdomain :: UvBounds -> Set2D Unitless (Line2D Unitless) -> Fuzzy Bool
-includeSubdomain subdomain edgeSet = Tolerance.using Quantity.zero $
-  case edgeSet of
-    Set2D.Node nodeBounds leftChild rightChild
-      | not (subdomain `intersects` nodeBounds) -> Resolved True
-      | smallerThan nodeBounds subdomain -> do
-          includeLeft <- includeSubdomain subdomain leftChild
-          includeRight <- includeSubdomain subdomain rightChild
-          Resolved (includeLeft && includeRight)
-      | otherwise -> Unresolved
-    Set2D.Leaf edgeBounds _
-      | not (subdomain `intersects` edgeBounds) -> Resolved True
-      | smallerThan edgeBounds subdomain -> Resolved False
-      | otherwise -> Unresolved
-
-smallerThan :: Bounds2D units -> Bounds2D units -> Bool
-smallerThan bounds1 bounds2 = Bounds2D.diameter bounds2 <= Bounds2D.diameter bounds1
