0dd91b835819c29dcb47dd8a5f66938e28c92274
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-04-15 19:11:15 -0400

    Add SurfaceCurve3D.[bounds,uvBounds]

--- opensolid-core/src/OpenSolid/SurfaceCurve3D.hs
+++ opensolid-core/src/OpenSolid/SurfaceCurve3D.hs
@@ -4,13 +4,19 @@ module OpenSolid.SurfaceCurve3D
   , curve
   , uvCurve
   , surfaceFunction
+  , bounds
+  , uvBounds
   )
 where
 
+import OpenSolid.Bounds3D (Bounds3D)
 import OpenSolid.Curve2D (Curve2D)
+import OpenSolid.Curve2D qualified as Curve2D
 import OpenSolid.Curve3D (Curve3D)
+import OpenSolid.Curve3D qualified as Curve3D
 import OpenSolid.Prelude
 import OpenSolid.SurfaceFunction3D (SurfaceFunction3D)
+import OpenSolid.UvBounds (UvBounds)
 
 data SurfaceCurve3D space = SurfaceCurve3D
   { surfaceFunction :: SurfaceFunction3D space
@@ -34,3 +40,9 @@ uvCurve = (.uvCurve)
 
 surfaceFunction :: SurfaceCurve3D space -> SurfaceFunction3D space
 surfaceFunction = (.surfaceFunction)
+
+bounds :: SurfaceCurve3D space -> Bounds3D space
+bounds = Curve3D.overallBounds . curve
+
+uvBounds :: SurfaceCurve3D space -> UvBounds
+uvBounds = Curve2D.overallBounds . uvCurve
