52adcab032a749fda064eb5fccd328d760226af1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-05-06 11:22:27 -0400

    Fix arc length parameterization to support curves with endpoint singularities
    dt/ds is infinite at singular endpoints (since ds/dt is zero),
    so approximate t(s) using the square root of a cubic spline
    instead of a plain quintic spline (which can't properly reproduce an infinite start or end derivative)

--- opensolid-core/src/OpenSolid/ArcLength.hs
+++ opensolid-core/src/OpenSolid/ArcLength.hs
@@ -1,29 +1,43 @@
 module OpenSolid.ArcLength (parameterization) where
 
 import OpenSolid.Bezier qualified as Bezier
+import OpenSolid.Interval qualified as Interval
 import OpenSolid.Lobatto qualified as Lobatto
 import OpenSolid.Number qualified as Number
 import OpenSolid.Prelude
 import OpenSolid.Quantity (Quantity (Q#))
 import OpenSolid.Quantity qualified as Quantity
+import OpenSolid.Tolerance qualified as Tolerance
 import OpenSolid.Unboxed.Math
 
 data Tree units
   = Node (Quantity units) (Tree units) (Tree units)
   | Leaf (Quantity units) Double# Double# Double# Double# Double# Double#
+  | SingularStart (Quantity units) Double# Double# Double# Double#
+  | SingularEnd (Quantity units) Double# Double# Double# Double#
 
 parameterization ::
   (Number -> Quantity units) ->
   (Number -> Quantity units) ->
   (Quantity units, Quantity units -> Number)
-parameterization dldt d2ldt2 = do
-  let dldt1 = dldt 0.0
-  let dldt2 = dldt Lobatto.p2
-  let dldt3 = dldt Lobatto.p3
-  let dldt4 = dldt 1.0
-  let coarseEstimate = Lobatto.estimate dldt1 dldt2 dldt3 dldt4
-  let (length, tree) = build 1 dldt d2ldt2 0.0 1.0 dldt1 dldt4 coarseEstimate
-  (length, evaluate tree)
+parameterization dsdt d2sdt2 = do
+  let dsdt1 = dsdt 0.0
+  let dsdt2 = dsdt Lobatto.p2
+  let dsdt3 = dsdt Lobatto.p3
+  let dsdt4 = dsdt 1.0
+  let dsdtMin = dsdt1 `min` dsdt2 `min` dsdt3 `min` dsdt4
+  let dsdtMax = dsdt1 `max` dsdt2 `max` dsdt3 `max` dsdt4
+  let dsdtTolerance = Tolerance.unitless * dsdtMax -- Reasonable tolerance for comparing ds/dt values
+  let (isConstant, singular0, singular1) =
+        Tolerance.using dsdtTolerance do
+          (dsdtMin ~= dsdtMax, dsdt1 ~= Quantity.zero, dsdt4 ~= Quantity.zero)
+  if isConstant
+    then (dsdt1, (/ dsdt1)) -- Constant-speed curve (e.g. line or arc)
+    else do
+      let initialEstimate = Lobatto.estimate dsdt1 dsdt2 dsdt3 dsdt4
+      let (length, tree) =
+            buildTree 1 dsdt d2sdt2 singular0 singular1 0.0 1.0 dsdt1 dsdt4 initialEstimate
+      (length, evaluate tree)
 
 evaluate :: Tree units -> Quantity units -> Number
 evaluate tree length = case tree of
@@ -31,43 +45,74 @@ evaluate tree length = case tree of
     | length < leftLength -> evaluate leftTree length
     | otherwise -> evaluate rightTree (length - leftLength)
   Leaf segmentLength t1# t2# t3# t4# t5# t6# -> do
-    let !(Q# s#) = length / segmentLength
-    Q# (quinticBezier# t1# t2# t3# t4# t5# t6# s#)
+    let !(Q# r#) = Quantity.clampTo Interval.unit (length / segmentLength)
+    Q# (quinticBezier# t1# t2# t3# t4# t5# t6# r#)
+  SingularStart segmentLength q1# q2# q3# q4# -> do
+    let !(Q# r#) = Quantity.clampTo Interval.unit (length / segmentLength)
+    Q# (sqrt# (cubicBezier# q1# q2# q3# q4# r#))
+  SingularEnd segmentLength q1# q2# q3# q4# -> do
+    let !(Q# r#) = 1.0 - Quantity.clampTo Interval.unit (length / segmentLength)
+    Q# (1.0## -# sqrt# (cubicBezier# q1# q2# q3# q4# r#))
 
-build ::
+buildTree ::
   Int ->
   (Number -> Quantity units) ->
   (Number -> Quantity units) ->
+  Bool ->
+  Bool ->
   Number ->
   Number ->
   Quantity units ->
   Quantity units ->
   Quantity units ->
   (Quantity units, Tree units)
-build level dldt d2ldt2 tStart tEnd dldtStart dldtEnd coarseEstimate = do
+buildTree n dsdt d2sdt2 singular0 singular1 tStart tEnd dsdtStart dsdtEnd coarseEstimate = do
   let tMid = Number.midpoint tStart tEnd
-  let dldtMid = dldt tMid
-  let dldtLeft2 = dldt (Number.interpolateFrom tStart tMid Lobatto.p2)
-  let dldtLeft3 = dldt (Number.interpolateFrom tStart tMid Lobatto.p3)
-  let dldtRight2 = dldt (Number.interpolateFrom tMid tEnd Lobatto.p2)
-  let dldtRight3 = dldt (Number.interpolateFrom tMid tEnd Lobatto.p3)
+  let dsdtMid = dsdt tMid
+  let dsdtLeft2 = dsdt (Number.interpolateFrom tStart tMid Lobatto.p2)
+  let dsdtLeft3 = dsdt (Number.interpolateFrom tStart tMid Lobatto.p3)
+  let dsdtRight2 = dsdt (Number.interpolateFrom tMid tEnd Lobatto.p2)
+  let dsdtRight3 = dsdt (Number.interpolateFrom tMid tEnd Lobatto.p3)
   let halfWidth = 0.5 * (tEnd - tStart)
-  let leftEstimate = halfWidth * Lobatto.estimate dldtStart dldtLeft2 dldtLeft3 dldtMid
-  let rightEstimate = halfWidth * Lobatto.estimate dldtMid dldtRight2 dldtRight3 dldtEnd
+  let leftEstimate = halfWidth * Lobatto.estimate dsdtStart dsdtLeft2 dsdtLeft3 dsdtMid
+  let rightEstimate = halfWidth * Lobatto.estimate dsdtMid dsdtRight2 dsdtRight3 dsdtEnd
   let fineEstimate = leftEstimate + rightEstimate
-  if level >= 10 || Quantity.abs (fineEstimate - coarseEstimate) <= 1e-12 * fineEstimate
+  let converged = Quantity.abs (fineEstimate - coarseEstimate) <= 1e-12 * fineEstimate
+  let singularStart = singular0 && tStart == 0.0
+  let singularEnd = singular1 && tEnd == 1.0
+  if (n >= 10 || converged) && not (singularStart && singularEnd)
     then do
+      let d2sdt2Start = d2sdt2 tStart
+      let d2sdt2End = d2sdt2 tEnd
       let segmentLength = fineEstimate
-      let dtdsStart = segmentLength / dldtStart
-      let dtdsEnd = segmentLength / dldtEnd
-      let d2tds2Start = -segmentLength ?*? d2ldt2 tStart * dtdsStart / Quantity.squared_ dldtStart
-      let d2tds2End = -segmentLength ?*? d2ldt2 tEnd * dtdsEnd / Quantity.squared_ dldtEnd
-      let !(Q# t1#, Q# t2#, Q# t3#, Q# t4#, Q# t5#, Q# t6#) =
-            Bezier.quinticHermite tStart dtdsStart d2tds2Start tEnd dtdsEnd d2tds2End
-      (segmentLength, Leaf segmentLength t1# t2# t3# t4# t5# t6#)
+      if
+        | singularStart -> do
+            let qEnd = Quantity.squared tEnd
+            let dqdrStart = 2.0 * segmentLength / d2sdt2Start
+            let dqdrEnd = 2.0 * tEnd * segmentLength / dsdtEnd
+            let !(Q# q1#, Q# q2#, Q# q3#, Q# q4#) =
+                  Bezier.cubicHermite 0.0 dqdrStart qEnd dqdrEnd
+            (segmentLength, SingularStart segmentLength q1# q2# q3# q4#)
+        | singularEnd -> do
+            let qStart = Quantity.squared (1.0 - tStart)
+            let dqdrStart = 2.0 * (1.0 - tStart) * segmentLength / dsdtStart
+            let dqdrEnd = -2.0 * segmentLength / d2sdt2End
+            let !(Q# q1#, Q# q2#, Q# q3#, Q# q4#) =
+                  Bezier.cubicHermite 0.0 dqdrEnd qStart dqdrStart
+            (segmentLength, SingularEnd segmentLength q1# q2# q3# q4#)
+        | otherwise -> do
+            let dtdrStart = segmentLength / dsdtStart
+            let dtdrEnd = segmentLength / dsdtEnd
+            let d2tdr2Start =
+                  -segmentLength ?*? d2sdt2Start * dtdrStart / Quantity.squared_ dsdtStart
+            let d2tdr2End =
+                  -segmentLength ?*? d2sdt2End * dtdrEnd / Quantity.squared_ dsdtEnd
+            let !(Q# t1#, Q# t2#, Q# t3#, Q# t4#, Q# t5#, Q# t6#) =
+                  Bezier.quinticHermite tStart dtdrStart d2tdr2Start tEnd dtdrEnd d2tdr2End
+            (segmentLength, Leaf segmentLength t1# t2# t3# t4# t5# t6#)
     else do
       let (leftLength, leftTree) =
-            build (level + 1) dldt d2ldt2 tStart tMid dldtStart dldtMid leftEstimate
+            buildTree (n + 1) dsdt d2sdt2 singular0 singular1 tStart tMid dsdtStart dsdtMid leftEstimate
       let (rightLength, rightTree) =
-            build (level + 1) dldt d2ldt2 tMid tEnd dldtMid dldtEnd rightEstimate
+            buildTree (n + 1) dsdt d2sdt2 singular0 singular1 tMid tEnd dsdtMid dsdtEnd rightEstimate
       (leftLength + rightLength, Node leftLength leftTree rightTree)
