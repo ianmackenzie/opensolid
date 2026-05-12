5ae2cf9ea86a445547a46dd4db986a7c19a01385
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-05-11 22:10:54 -0400

    Add initial VectorTransform.Exists typeclass

--- opensolid-core/src/OpenSolid/VectorTransform.hs
+++ opensolid-core/src/OpenSolid/VectorTransform.hs
@@ -1,8 +1,18 @@
-module OpenSolid.VectorTransform (VectorTransform, VectorTransform1D (VectorTransform1D)) where
+module OpenSolid.VectorTransform
+  ( VectorTransform
+  , VectorTransform1D (VectorTransform1D)
+  , Exists
+  , toAffine
+  )
+where
 
+import Data.Coerce qualified
 import Data.Void (Void)
 import OpenSolid.Prelude
 import OpenSolid.Primitives (VectorTransform2D, VectorTransform3D)
+import {-# SOURCE #-} OpenSolid.Transform qualified as Transform
+import OpenSolid.VectorTransform2D qualified as VectorTransform2D
+import OpenSolid.VectorTransform3D qualified as VectorTransform3D
 
 type family
   VectorTransform dimension tag space =
@@ -13,3 +23,17 @@ type family
   VectorTransform 3 tag space = VectorTransform3D tag space
 
 newtype VectorTransform1D tag = VectorTransform1D Number
+
+class Exists dimension space where
+  toAffine ::
+    VectorTransform dimension tag space ->
+    VectorTransform dimension Transform.Affine space
+
+instance Exists 1 Void where
+  toAffine = Data.Coerce.coerce
+
+instance Exists 2 Void where
+  toAffine = VectorTransform2D.toAffine
+
+instance Exists 3 space where
+  toAffine = VectorTransform3D.toAffine
