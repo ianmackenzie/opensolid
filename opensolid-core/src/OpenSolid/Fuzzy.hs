a09f0dab6e677bc469111059ee8e257613248ec8
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-06-20 13:52:44 -0400

    Add Fuzz.orFail function

--- opensolid-core/src/OpenSolid/Fuzzy.hs
+++ opensolid-core/src/OpenSolid/Fuzzy.hs
@@ -4,10 +4,12 @@ module OpenSolid.Fuzzy
   , oneOf
   , map
   , fromMaybe
+  , orFail
   )
 where
 
 import OpenSolid.Prelude
+import OpenSolid.Text qualified as Text
 import Prelude qualified
 
 fromMaybe :: Maybe a -> Fuzzy a
@@ -25,3 +27,7 @@ oneOf fuzzies = case fuzzies of
   Resolved value : _ -> Resolved value
   Unresolved : rest -> oneOf rest
   [] -> Unresolved
+
+orFail :: MonadFail m => Text -> Fuzzy a -> m a
+orFail _ (Resolved value) = Prelude.return value
+orFail message Unresolved = Prelude.fail (Text.unpack message)
