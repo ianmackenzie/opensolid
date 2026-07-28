4d82d013d42be504fe3445c8c1ffcd31d0dfdff3
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-07-28 13:05:46 -0400

    Remove unused code

--- opensolid-core/src/OpenSolid/SearchDomain.hs
+++ opensolid-core/src/OpenSolid/SearchDomain.hs
@@ -1,8 +1,6 @@
 module OpenSolid.SearchDomain
   ( Bounds
-  , touching
   , contains
-  , overlapping
   )
 where
 
@@ -15,22 +13,12 @@ import OpenSolid.UvBounds (UvBounds)
 
 class (Set.Bounds bounds, Intersects bounds bounds (Tolerance Unitless)) => Bounds bounds where
   contains :: bounds -> bounds -> Bool
-  overlap :: bounds -> bounds -> Number
 
 instance Bounds (Interval Unitless) where
   contains = Interval.contains
-  overlap = Interval.overlap
 
 instance Bounds UvBounds where
   contains = Bounds2D.contains
-  overlap = Bounds2D.overlap
 
 instance (Bounds bounds1, Bounds bounds2) => Bounds (bounds1, bounds2) where
   contains (b1, b2) (a1, a2) = contains b1 a1 && contains b2 a2
-  overlap (b1, b2) (a1, a2) = min (overlap b1 a1) (overlap b2 a2)
-
-touching :: Bounds b => b -> b -> Bool
-touching bounds1 bounds2 = overlap bounds1 bounds2 >= 0.0
-
-overlapping :: Bounds b => b -> b -> Bool
-overlapping bounds1 bounds2 = overlap bounds1 bounds2 > 0.0
