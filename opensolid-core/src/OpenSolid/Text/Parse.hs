65bdc592f309176206e9dfc7f59f0b0d22db1310
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-07-28 14:13:58 -0400

    Switch from Data.Text.Read.double to Data.Text.Read.rational
    More accurate (although slower),
    and avoids need for a Result.map since Number has a Rational instanace

--- opensolid-core/src/OpenSolid/Text/Parse.hs
+++ opensolid-core/src/OpenSolid/Text/Parse.hs
@@ -4,7 +4,6 @@ import Data.Text qualified
 import Data.Text.Read (Reader)
 import Data.Text.Read qualified
 import OpenSolid.Prelude
-import OpenSolid.Result qualified as Result
 import Prelude qualified
 
 num :: Prelude.Num a => Reader a -> Text -> Result Text a
@@ -26,4 +25,4 @@ int :: Text -> Result Text Int
 int = num Data.Text.Read.decimal
 
 number :: Text -> Result Text Number
-number = Result.map Quantity . num Data.Text.Read.double
+number = num Data.Text.Read.rational
