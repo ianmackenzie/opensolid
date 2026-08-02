fc6526cc3d4f1857117fef56671db3142fca3b9e
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-08-01 21:20:35 -0400

    Simplify VectorSurfaceFunction3D.Nondegenerate.direction slightly

--- opensolid-core/src/OpenSolid/VectorSurfaceFunction3D/Nondegenerate.hs
+++ opensolid-core/src/OpenSolid/VectorSurfaceFunction3D/Nondegenerate.hs
@@ -1,11 +1,11 @@
 module OpenSolid.VectorSurfaceFunction3D.Nondegenerate (direction) where
 
 import OpenSolid.Direction3D (Direction3D)
-import OpenSolid.Direction3D qualified as Direction3D
 import OpenSolid.Nondegenerate (Nondegenerate (Nondegenerate))
+import OpenSolid.Nonzero (Nonzero (Nonzero))
 import OpenSolid.Prelude
 import OpenSolid.UvPoint (UvPoint, data UvPoint)
-import OpenSolid.Vector3D qualified as Vector3D
+import OpenSolid.Vector3D.Nonzero qualified as Vector3D.Nonzero
 import OpenSolid.VectorSurfaceFunction3D (VectorSurfaceFunction3D)
 import OpenSolid.VectorSurfaceFunction3D qualified as VectorSurfaceFunction3D
 
@@ -18,10 +18,10 @@ direction (Nondegenerate function) uvPoint = do
   let UvPoint uValue vValue = uvPoint
   let fValue = VectorSurfaceFunction3D.valueAt uvPoint function
   let (fuValue, fvValue) = VectorSurfaceFunction3D.partialDerivativesAt uvPoint function
-  let nonZeroVector
-        | uValue == 0.0 && VectorSurfaceFunction3D.degenerateLeft function = fuValue
-        | uValue == 1.0 && VectorSurfaceFunction3D.degenerateRight function = -fuValue
-        | vValue == 0.0 && VectorSurfaceFunction3D.degenerateBottom function = fvValue
-        | vValue == 1.0 && VectorSurfaceFunction3D.degenerateTop function = -fvValue
-        | otherwise = fValue
-  Direction3D.unsafe (nonZeroVector / Vector3D.magnitude nonZeroVector)
+  Vector3D.Nonzero.direction . Nonzero $
+    if
+      | uValue == 0.0 && VectorSurfaceFunction3D.degenerateLeft function -> fuValue
+      | uValue == 1.0 && VectorSurfaceFunction3D.degenerateRight function -> -fuValue
+      | vValue == 0.0 && VectorSurfaceFunction3D.degenerateBottom function -> fvValue
+      | vValue == 1.0 && VectorSurfaceFunction3D.degenerateTop function -> -fvValue
+      | otherwise -> fValue
