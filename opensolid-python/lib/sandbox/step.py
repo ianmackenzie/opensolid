9af642cc2845569e3e6de7622ac2e76d311149e7
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2026-08-01 21:20:35 -0400

    Update test script to use new function names

--- opensolid-python/lib/sandbox/step.py
+++ opensolid-python/lib/sandbox/step.py
@@ -15,68 +15,79 @@ header = Step.header(
     schema_identifiers=["AUTOMOTIVE_DESIGN { 1 0 10303 214 2 1 1}"],
 )
 application_context = Step.entity(
-    "APPLICATION_CONTEXT", [Step.text("mechanical design")]
+    "APPLICATION_CONTEXT", [Step.text_attribute("mechanical design")]
 )
 application_protocol_definition = Step.entity(
     "APPLICATION_PROTOCOL_DEFINITION",
     [
-        Step.text(""),
-        Step.text("automotive_design"),
-        Step.int(2003),
+        Step.text_attribute(""),
+        Step.text_attribute("automotive_design"),
+        Step.int_attribute(2003),
         Step.reference_to(application_context),
     ],
 )
 product_context = Step.entity(
     "PRODUCT_CONTEXT",
     [
-        Step.text(""),
+        Step.text_attribute(""),
         Step.reference_to(application_context),
-        Step.text(""),
+        Step.text_attribute(""),
     ],
 )
 product = Step.entity(
     "PRODUCT",
     [
-        Step.text("A0001"),
-        Step.text("Test Part 1"),
-        Step.text(""),
-        Step.list([Step.reference_to(product_context)]),
+        Step.text_attribute("A0001"),
+        Step.text_attribute("Test Part 1"),
+        Step.text_attribute(""),
+        Step.list_attribute([Step.reference_to(product_context)]),
     ],
 )
 product_definition_formation = Step.entity(
     "PRODUCT_DEFINITION_FORMATION",
-    [Step.text("1"), Step.null, Step.reference_to(product)],
+    [Step.text_attribute("1"), Step.null_attribute, Step.reference_to(product)],
 )
 product_definition_context = Step.entity(
     "PRODUCT_DEFINITION_CONTEXT",
     [
-        Step.text("part definition"),
+        Step.text_attribute("part definition"),
         Step.reference_to(application_context),
-        Step.text("manufacturing"),
+        Step.text_attribute("manufacturing"),
     ],
 )
 organization = Step.entity(
-    "ORGANIZATION", [Step.text("O0001"), Step.text("LKSoft"), Step.text("company")]
+    "ORGANIZATION",
+    [
+        Step.text_attribute("O0001"),
+        Step.text_attribute("LKSoft"),
+        Step.text_attribute("company"),
+    ],
 )
 product_definition = Step.entity(
     "PRODUCT_DEFINITION",
     [
-        Step.text("0"),
-        Step.null,
+        Step.text_attribute("0"),
+        Step.null_attribute,
         Step.reference_to(product_definition_formation),
         Step.reference_to(product_definition_context),
     ],
 )
 product_related_product_category = Step.entity(
     "PRODUCT_RELATED_PRODUCT_CATEGORY",
-    [Step.text("part"), Step.null, Step.list([Step.reference_to(product)])],
+    [
+        Step.text_attribute("part"),
+        Step.null_attribute,
+        Step.list_attribute([Step.reference_to(product)]),
+    ],
 )
 applied_organization_assignment = Step.entity(
     "APPLIED_ORGANIZATION_ASSIGNMENT",
     [
         Step.reference_to(organization),
-        Step.reference_to(Step.entity("ORGANIZATION_ROLE", [Step.text("id owner")])),
-        Step.list([Step.reference_to(product)]),
+        Step.reference_to(
+            Step.entity("ORGANIZATION_ROLE", [Step.text_attribute("id owner")])
+        ),
+        Step.list_attribute([Step.reference_to(product)]),
     ],
 )
 model = Step.model(
